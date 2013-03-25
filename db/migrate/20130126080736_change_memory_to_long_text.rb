ac67a5de15e0bcab8ce5ff3c01100620ef577fff
Andrew Cantino <cantino@gmail.com>
2013-03-24 20:32:53 -0700

    Only increase text field size for mysql.

--- db/migrate/20130126080736_change_memory_to_long_text.rb
+++ db/migrate/20130126080736_change_memory_to_long_text.rb
@@ -1,11 +1,21 @@
+# PG allows arbitrarily long text fields but MySQL has default limits. Make those limits larger if we're using MySQL.
+
 class ChangeMemoryToLongText < ActiveRecord::Migration
   def up
-    change_column :agents, :memory, :text, :limit => 4294967295
-    change_column :events, :payload, :text, :limit => 16777215
+    if mysql?
+      change_column :agents, :memory, :text, :limit => 4294967295
+      change_column :events, :payload, :text, :limit => 16777215
+    end
   end
 
   def down
-    change_column :agents, :memory, :text, :limit => 65535
-    change_column :events, :payload, :text, :limit => 65535
+    if mysql?
+      change_column :agents, :memory, :text, :limit => 65535
+      change_column :events, :payload, :text, :limit => 65535
+    end
+  end
+
+  def mysql?
+    ActiveRecord::Base.connection.adapter_name =~ /mysql/i
   end
 end
