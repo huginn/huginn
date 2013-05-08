5d63df8d18b237285fd1a796140428a73dbf9b24
Rishabh Jain <rishabhj@live.in>
2013-05-08 22:36:06 +0530

    Added JSONPath

--- app/models/agents/sentiment_value_agent.rb
+++ app/models/agents/sentiment_value_agent.rb
@@ -3,17 +3,18 @@ require 'csv'
 module Agents
     class SentimentValueAgent < Agent
         description <<-MD
-            The SentimentValueAgent generates `good-bad` (psychological valence or happiness index),`active-passive` (arousal), and `strong-weak` (dominance) score. It will output a value between 1 and 9. Make sure the content this agent is running on have sufficient length.
-            Add more stuff
+            The SentimentValueAgent generates `good-bad` (psychological valence or happiness index), `active-passive` (arousal), and  `strong-weak` (dominance) score. It will output a value between 1 and 9. Make sure the content this agent is analyzing have sufficient length to get respectable results.
+
+            Provide a JSONPath in `content` field where content is residing and set `expected_receive_period_in_days` to the maximum number of days you would allow to be passed between events being received by this agent.
         MD
 
         event_description <<-MD
             Events look like:
             {
-                :content   => "The quick brown fox jumps over the lazy dog."
-                :valence   => 6.199999
-                :arousal   => 5.432212
-                :dominance => 4.346312
+                :content   => "The quick brown fox jumps over the lazy dog.",
+                :valence   => 6.196666666666666,
+                :arousal   => 4.993333333333333,
+                :dominance => 5.63
             }
         MD
 
@@ -21,12 +22,13 @@ module Agents
 
         def default_options
             {
-
+                :content => "$.message.text",
+                :expected_receive_period_in_days => 1
             }
         end
 
         def working?
-            true
+            last_receive_at && last_receive_at > options[:expected_receive_period_in_days].to_i.days.ago
         end
 
         def receive(incoming_events)
@@ -37,12 +39,13 @@ module Agents
         end
 
         def validate_options
+            errors.add(:base, "content and expected_receive_period_in_days must be present") unless options[:content].present? && options[:expected_receive_period_in_days].present?
         end
 
         def sentiment_hash
             anew = {}
-            CSV.foreach Rails.root.join('app/assets/anew.csv') do |row|
-                anew[row[0]] = [row[2],row[4],row[6]].map {|val| val.to_f}
+            CSV.foreach Rails.root.join('data/anew.csv') do |row|
+                anew[row[0]] = row.values_at(2,4,6).map {|val| val.to_f}
             end
             anew
         end
@@ -68,9 +71,10 @@ module Agents
             if self.memory[:queue] && self.memory[:queue].length > 0
                 anew = sentiment_hash
                 self.memory[:queue].each do |text|
-                    if text[:message]
-                        sent_values = sentiment_values anew, text[:message]
-                        create_event :payload => {:content => text[:message], :valence => sent_values[0], :arousal => sent_values[1], :dominance => sent_values[2]}
+                    if Utils.values_at(text, options[:content])[0]
+                        content = Utils.values_at(text, options[:content])[0]
+                        sent_values = sentiment_values anew, content
+                        create_event :payload => {:content => content, :valence => sent_values[0], :arousal => sent_values[1], :dominance => sent_values[2]}
                     end
                 end
                 self.memory[:queue] = []
