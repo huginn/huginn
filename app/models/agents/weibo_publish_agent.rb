8754e50a022ce4738c03a02110391c01fce35fc1
Albert Sun <Albert.Sun@nytimes.com>
2013-05-15 21:15:02 -0400

    first pass at a weibo publishing agent, almost identical to the twitter one
    updating weibo agent with app_key/app_secret options to make it work. and some custom code for if the input event comes from twitter to unwrap t.co urls as those are not allowed on weibo
    
    better spec for weibo agent
    
    text edits
    
    changing user_id to uid

---
+++ app/models/agents/weibo_publish_agent.rb
@@ -0,0 +1,91 @@
+require "weibo_2"
+
+module Agents
+  class WeiboPublishAgent < Agent
+    cannot_be_scheduled!
+
+    description <<-MD
+      The WeiboPublishAgent publishes tweets from the events it receives.
+
+      You must first set up a Weibo app and generate an `acess_token` for the user to send statuses as.
+
+      Include that in options, along with the `app_key` and `app_secret` for your Weibo app. It's useful to also include the Weibo user id of the person to publish as.
+
+      You must also specify a `message_path` parameter: a [JSONPaths](http://goessner.net/articles/JsonPath/) to the value to tweet.
+
+      Set `expected_update_period_in_days` to the maximum amount of time that you'd expect to pass between Events being created by this Agent.
+    MD
+
+    def validate_options
+      unless options[:uid].present? &&
+        options[:expected_update_period_in_days].present? &&
+        options[:app_key].present? &&
+        options[:app_secret].present? &&
+        options[:access_token].present?
+        errors.add(:base, "expected_update_period_in_days, uid, and access_token are required")
+      end
+    end
+
+    def working?
+      (event = event_created_within(options[:expected_update_period_in_days].to_i.days)) && event.payload.present? && event.payload[:success] == true
+    end
+
+    def default_options
+      {
+          :uid => "",
+          :access_token => "---",
+          :app_key => "---",
+          :app_secret => "---",
+          :expected_update_period_in_days => "10",
+          :message_path => "text"
+      }
+    end
+
+    def receive(incoming_events)
+      # if there are too many, dump a bunch to avoid getting rate limited
+      if incoming_events.count > 20
+        incoming_events = incoming_events.first(20)
+      end
+      incoming_events.each do |event|
+        tweet_text = Utils.value_at(event.payload, options[:message_path])
+        if event.agent.type == "Agents::TwitterUserAgent"
+          tweet_text = unwrap_tco_urls(tweet_text, event.payload)
+        end
+        begin
+          publish_tweet tweet_text
+          create_event :payload => {
+            :success => true,
+            :published_tweet => tweet_text,
+            :agent_id => event.agent_id,
+            :event_id => event.id
+          }
+        rescue OAuth2::Error => e
+          create_event :payload => {
+            :success => false,
+            :error => e.message,
+            :failed_tweet => tweet_text,
+            :agent_id => event.agent_id,
+            :event_id => event.id
+          }
+        end
+      end
+    end
+
+    def publish_tweet text
+      WeiboOAuth2::Config.api_key = options[:app_key] # WEIBO_APP_KEY
+      WeiboOAuth2::Config.api_secret = options[:app_secret] # WEIBO_APP_SECRET
+      client = WeiboOAuth2::Client.new
+      client.get_token_from_hash :access_token => options[:access_token]
+
+      client.statuses.update text
+    end
+
+    def unwrap_tco_urls text, tweet_json
+      tweet_json[:entities][:urls].each do |url|
+        text.gsub! url[:url], url[:expanded_url]
+      end
+      return text
+    end
+
+  end
+end
---
+++ spec/models/agents/weibo_publish_agent_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe Agents::WeiboPublishAgent do
+  before do
+    @opts = {
+      :uid => "1234567",
+      :expected_update_period_in_days => "2",
+      :app_key => "---",
+      :app_secret => "---",
+      :access_token => "---",
+      :message_path => "text"
+    }
+
+    @checker = Agents::WeiboPublishAgent.new(:name => "Weibo Publisher", :options => @opts)
+    @checker.user = users(:bob)
+    @checker.save!
+
+    @event = Event.new
+    @event.agent = agents(:bob_weather_agent)
+    @event.payload = { :text => 'Gonna rain..' }
+    @event.save!
+
+    @sent_messages = []
+    stub.any_instance_of(Agents::WeiboPublishAgent).publish_tweet { |message| @sent_messages << message}
+  end
+
+  describe '#receive' do
+    it 'should publish any payload it receives' do
+      event1 = Event.new
+      event1.agent = agents(:bob_rain_notifier_agent)
+      event1.payload = { :text => 'Gonna rain..' }
+      event1.save!
+
+      event2 = Event.new
+      event2.agent = agents(:bob_weather_agent)
+      event2.payload = { :text => 'More payload' }
+      event2.save!
+
+      Agents::WeiboPublishAgent.async_receive(@checker.id, [event1.id, event2.id])
+      @sent_messages.count.should eq(2)
+      @checker.events.count.should eq(2)
+    end
+  end
+
+  describe '#receive a tweet' do
+    it 'should publish a tweet after expanding any t.co urls' do
+      event = Event.new
+      event.agent = agents(:bob_twitter_user_agent)
+      event.payload = JSON.parse(File.read(Rails.root.join("spec/data_fixtures/one_tweet.json")))
+      event.save!
+
+      Agents::WeiboPublishAgent.async_receive(@checker.id, [event.id])
+      @sent_messages.count.should eq(1)
+      @checker.events.count.should eq(1)
+      @sent_messages.first.include?("t.co").should_not be_true
+    end
+  end
+
+  describe '#working?' do
+    it 'checks if events have been received within the expected receive period' do
+      @checker.should_not be_working # No events received
+      Agents::WeiboPublishAgent.async_receive(@checker.id, [@event.id])
+      @checker.reload.should be_working # Just received events
+      two_days_from_now = 2.days.from_now
+      stub(Time).now { two_days_from_now }
+      @checker.reload.should_not be_working # More time has passed than the expected receive period without any new events
+    end
+  end
+end
