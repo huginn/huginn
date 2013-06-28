3261a066ced7302bc5ce935c91937d22ba3b6b57
Andrew Cantino <cantino@gmail.com>
2013-06-27 22:31:04 -0700

    switch an unless/else and fix indentation and language

--- app/models/agents/adioso_agent.rb
+++ app/models/agents/adioso_agent.rb
@@ -1,26 +1,27 @@
 module Agents
   class AdiosoAgent < Agent
 
-  cannot_receive_events!
+    cannot_receive_events!
 
-	default_schedule "every_1d"
+  	default_schedule "every_1d"
 
-  description <<-MD
-		Adioso Agent will tell you about the minimum airline prices between a pair of cities, and between a certain period of time.
-    Currency is USD, Please make sure difference between `start_date` and `end_date` is less than 150 days. You will need to contact [Adioso](http://adioso.com/)
-		for `username` and `password`.
-  MD
+    description <<-MD
+  		The Adioso Agent will tell you the minimum airline prices between a pair of cities, and within a certain period of time.
+      The currency is USD. Please make sure that the difference between `start_date` and `end_date` is less than 150 days. You will need to contact [Adioso](http://adioso.com/)
+  		for a `username` and `password`.
+    MD
 
-  event_description <<-MD
+    event_description <<-MD
+      If flights are present then events look like:
 
-    If flights are present then events look like:
-			{ "cost" : 75.23,
-        "date" : "June 25, 2013",
-			  "route" : "New York to Chicago" }
+  		    { "cost" : 75.23,
+            "date" : "June 25, 2013",
+  			    "route" : "New York to Chicago" }
 
-    otherwise
-     { "nonetodest" : "No flights found to the specified destination" }
-  MD
+      otherwise
+    
+          { "nonetodest" : "No flights found to the specified destination" }
+    MD
 
     def default_options
       {
@@ -53,15 +54,15 @@ module Agents
       parse_response = HTTParty.get "http://api.adioso.com/v2/search/parse?q=#{URI.encode(options[:from])}+to+#{URI.encode(options[:to])}", auth_options
       fare_request = parse_response["search_url"].gsub /(end=)(\d*)([^\d]*)(\d*)/, "\\1#{date_to_unix_epoch(options[:end_date])}\\3#{date_to_unix_epoch(options[:start_date])}"
       fare = HTTParty.get fare_request, auth_options
-			unless fare["warnings"]
+
+			if fare["warnings"]
+				create_event :payload => fare["warnings"]
+			else
 				event = fare["results"].min {|a,b| a["cost"] <=> b["cost"]}
 				event["date"]  = Time.at(event["date"]).to_date.httpdate[0..15]
 				event["route"] = "#{options[:from]} to #{options[:to]}" 
 				create_event :payload => event
-			else
-				create_event :payload => fare["warnings"]
 			end
-				
     end
   end
 end
