a521695d6cd55817853b8510c02365e124b64eee
Albert Sun <Albert.Sun@nytimes.com>
2013-09-24 12:17:38 -0400

    initialize_on_precompile to false for asset compile

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,8 @@ module Huginn
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-
+    config.assets.initialize_on_precompile = false
+    
     # Enable the asset pipeline
     config.assets.enabled = true
 
