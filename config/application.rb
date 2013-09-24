198708baa5f88adb86698331895da77fee88fd3f
Albert Sun <Albert.Sun@nytimes.com>
2013-09-24 12:43:55 -0400

    adding ASSET_HOST config and a staging environment file

--- config/application.rb
+++ config/application.rb
@@ -52,10 +52,10 @@ module Huginn
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-    config.assets.initialize_on_precompile = false
-    
+
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
