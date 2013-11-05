f32c6f0f7c34f6430786f62b593deaef68a3b550
Andrew Cantino <cantino@gmail.com>
2013-11-04 22:28:32 -0800

    add a transaction around event receiving

--- bin/schedule.rb
+++ bin/schedule.rb
@@ -10,57 +10,64 @@ end
 
 require 'rufus/scheduler'
 
-def run_schedule(time, mutex)
-  ActiveRecord::Base.connection_pool.with_connection do
-    mutex.synchronize do
-      puts "Queuing schedule for #{time}"
-      Agent.delay.run_schedule(time)
+class HuginnScheduler
+  def run_schedule(time, mutex)
+    ActiveRecord::Base.connection_pool.with_connection do
+      mutex.synchronize do
+        puts "Queuing schedule for #{time}"
+        Agent.delay.run_schedule(time)
+      end
     end
   end
-end
 
-def propogate!(mutex)
-  ActiveRecord::Base.connection_pool.with_connection do
-    mutex.synchronize do
-      puts "Queuing event propagation"
-      Agent.delay.receive!
+  def propagate!(mutex)
+    ActiveRecord::Base.connection_pool.with_connection do
+      mutex.synchronize do
+        puts "Queuing event propagation"
+        Agent.delay.receive!
+      end
     end
   end
-end
 
-mutex = Mutex.new
+  def run!
+    mutex = Mutex.new
 
-scheduler = Rufus::Scheduler.new
+    rufus_scheduler = Rufus::Scheduler.new
 
-# Schedule event propagation.
+    # Schedule event propagation.
 
-scheduler.every '5m' do
-  propogate!(mutex)
-end
-
-# Schedule repeating events.
-
-%w[2m 5m 10m 30m 1h 2h 5h 12h 1d 2d 7d].each do |schedule|
-  scheduler.every schedule do
-    run_schedule "every_#{schedule}", mutex
-  end
-end
-
-# Schedule events for specific times.
-
-# Times are assumed to be in PST for now.  Can store a user#timezone later.
-24.times do |hour|
-  scheduler.cron "0 #{hour} * * * America/Los_Angeles" do
-    if hour == 0
-      run_schedule "midnight", mutex
-    elsif hour < 12
-      run_schedule "#{hour}am", mutex
-    elsif hour == 12
-      run_schedule "noon", mutex
-    else
-      run_schedule "#{hour - 12}pm", mutex
+    rufus_scheduler.every '1m' do
+      propagate!(mutex)
     end
+
+    # Schedule repeating events.
+
+    %w[2m 5m 10m 30m 1h 2h 5h 12h 1d 2d 7d].each do |schedule|
+      rufus_scheduler.every schedule do
+        run_schedule "every_#{schedule}", mutex
+      end
+    end
+
+    # Schedule events for specific times.
+
+    # Times are assumed to be in PST for now.  Can store a user#timezone later.
+    24.times do |hour|
+      rufus_scheduler.cron "0 #{hour} * * * America/Los_Angeles" do
+        if hour == 0
+          run_schedule "midnight", mutex
+        elsif hour < 12
+          run_schedule "#{hour}am", mutex
+        elsif hour == 12
+          run_schedule "noon", mutex
+        else
+          run_schedule "#{hour - 12}pm", mutex
+        end
+      end
+    end
+
+    rufus_scheduler.join
   end
 end
 
-scheduler.join
+scheduler = HuginnScheduler.new
+scheduler.run!
