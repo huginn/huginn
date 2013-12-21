dff2bc68f4bf52958e3905873c8e9f0e88cd84f8
j-wilkins <pablo_honey@me.com>
2013-12-20 22:41:54 -1000

    configurable webhook payload , update description, improve tests

--- app/models/agents/webhook_agent.rb
+++ app/models/agents/webhook_agent.rb
@@ -2,34 +2,61 @@ module Agents
   class WebhookAgent < Agent
     cannot_be_scheduled!
 
-    description <<-MD
-      Use this Agent to create events by receiving webhooks from any source.
+    description  do
+        <<-MD
+        Use this Agent to create events by receiving webhooks from any source.
 
-      Options:
+        In order to create events with this agent, make a POST request to:
+        ```
+           https://#{ENV['DOMAIN']}/users/#{user.id}/webhooks/#{id || '<id>'}/:secret
+        ``` where `:secret` is specified in your options.
 
-        * `secret` - A token that the host will provide for authentication.
-    MD
+        The
+
+        Options:
+
+          * `secret` - A token that the host will provide for authentication.
+          * `expected_receive_period_in_days` - How often you expect to receive
+            events this way. Used to determine if the agent is working.
+          * `payload_path` - JSONPath of the attribute of the POST body to be
+            used as the Event payload.
+      MD
+    end
+
+    event_description do
+      <<-MD
+        The event payload is base on the value of the `payload_path` option,
+        which is set to `#{options[:payload_path]}`.
+      MD
+    end
 
     def default_options
-      { "secret" => "supersecretstring", }
+      { "secret" => "supersecretstring",
+        "expected_receive_period_in_days" => 1,
+        "payload_path" => "payload"}
     end
 
     def receive_webhook(params)
-      return ["Not Authorized", 401] unless params[:secret] == options[:secret]
+      secret = params.delete(:secret)
+      return ["Not Authorized", 401] unless secret == options[:secret]
 
-      create_event(:payload => params[:payload])
+      create_event(:payload => payload_for(params))
 
       ['Event Created', 201]
     end
 
     def working?
-      true
+      event_created_within(options[:expected_receive_period_in_days]) && !recent_error_logs?
     end
 
     def validate_options
       unless options[:secret].present?
         errors.add(:base, "Must specify a :secret for 'Authenticating' requests")
       end
+    end
+
+    def payload_for(params)
+      Utils.values_at(params, options[:payload_path]) || {}
     end
   end
 end
--- spec/models/agents/webhook_agent_spec.rb
+++ spec/models/agents/webhook_agent_spec.rb
@@ -3,11 +3,12 @@ require 'spec_helper'
 describe Agents::WebhookAgent do
   let(:agent) do
     _agent = Agents::WebhookAgent.new(:name => 'webhook',
-                             :options => {:secret => :foobar})
+             :options => {:secret => :foobar, :payload_path => '$'})
     _agent.user = users(:bob)
     _agent.save!
     _agent
   end
+  let(:payload) { {:some => :info} }
 
   after { agent.destroy }
 
@@ -15,17 +16,19 @@ describe Agents::WebhookAgent do
     it 'should create event if secret matches' do
       out = nil
       lambda {
-        out = agent.receive_webhook({:secret => :foobar, :payload => {:some => :info}})
+        out = agent.receive_webhook({:secret => :foobar, :payload => payload})
       }.should change { Event.count }.by(1)
       out.should eq(['Event Created', 201])
+      Event.last.last.payload.should eq([{'payload' => payload}])
     end
 
     it 'should not create event if secrets dont match' do
       out = nil
       lambda {
-        out = agent.receive_webhook({:secret => :bazbat, :payload => {:some => :info}})
+        out = agent.receive_webhook({:secret => :bazbat, :payload => payload})
       }.should change { Event.count }.by(0)
       out.should eq(['Not Authorized', 401])
+      Event.last.last.payload.should eq([{'payload' => payload}])
     end
   end
 end
