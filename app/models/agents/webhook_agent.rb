e4aa7f6f161d41bd961d05d21d900d68046a72ee
Wong Liang Zan <zan@liangzan.net>
2014-01-19 11:47:16 +0800

    method name mistake

--- app/models/agents/webhook_agent.rb
+++ app/models/agents/webhook_agent.rb
@@ -46,7 +46,7 @@ module Agents
     end
 
     def working?
-      event_created_within(options['expected_receive_period_in_days']) && !recent_error_logs?
+      event_created_within?(options['expected_receive_period_in_days']) && !recent_error_logs?
     end
 
     def validate_options
