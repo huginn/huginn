0f1490452c342c580bd5ef6ad1a37e1c1f64e553
Umar M. Sheikh <umar.m.sheikh@gmail.com>
2014-01-28 15:02:13 +0500

    a working code agent

--- app/models/agents/code_agent.rb
+++ app/models/agents/code_agent.rb
@@ -2,22 +2,21 @@ require 'date'
 require 'cgi'
 module Agents
   class CodeAgent < Agent
+    #cannot_receive_events!
+    #cannot_be_scheduled!
     description <<-MD
       Here is an agent that gives you the ability to specify your own code. We have already provided you
       a javascript object that has read access to this agent's memory, events, options and the attributes of the agent.
       We also provide you with a method to create events on the server.
       You will be provided with an instance of the Agent object in javascript, with access to the above data.
       You can create events based on your own logic.
-      Specifically, you have the following class
+      Specifically, you have the following class, lets say, present is a string "js_code".
 
           function Agent(m, e, o, agent){
           this.memory = JSON.parse(m);
           this.events = JSON.parse(e);
           this.options = JSON.parse(o);
           this.agent = JSON.parse(agent);
-          }
-          Agent.prototype.print_memory = function(){
-            return this.memory;
           }
           Agent.prototype.run = function(){
             //Implement me
@@ -29,7 +28,9 @@ module Agents
 
       We will yield control to your implementation in the following way:
 
+          context.eval(js_code); //this is the code that declares the class Agent, and provides a global create_event method.
           context.eval("a = new Agent(memory, events, options, agent)")
+          context.eval(options['code'])
           context.eval("a.run();")
 
       You need to provide the run() implementation, as well as other methods it may need to interact with.
@@ -47,11 +48,6 @@ module Agents
       return this.memory;
     }
     Agent.prototype.run = function(){
-      //have access to this.memory, this.events, this.options, and this.agent;
-      // do computation...
-      //...
-      var pd = JSON.stringify({memory: this.memory, events: this.events, options: this.options});
-      create_event(pd);
     }
     H
     end
@@ -69,6 +65,33 @@ module Agents
       string = "a = new Agent('#{m}','#{e}','#{o}','#{a}');"
       context.eval(string)
       context.eval("a.run();")
+    end
+    def check
+      context = V8::Context.new
+      context.eval(example_js)
+      context["create_event"] = lambda {|x,y| puts x; puts y; create_event payload: JSON.parse(y)}
+      context.eval(options['code']) # should override the run function.
+      a, m, e, o = [self.attributes.to_json, self.memory.to_json, "".to_json, self.options.to_json]
+      string = "a = new Agent('#{m}','#{e}','#{o}','#{a}');"
+      context.eval(string)
+      context.eval("a.run();")
+    end
+
+    def receive(incoming_events)
+      context = V8::Context.new
+      context.eval(example_js)
+      context.eval(options['code']) # should override the run function.
+      context["create_event"] = lambda {|x,y| puts x; puts y; create_event payload: JSON.parse(y)}
+      a, m, e, o = [self.attributes.to_json, self.memory.to_json, incoming_events.to_json, self.options.to_json]
+      string = "a = new Agent('#{m}','#{e}','#{o}','#{a}');"
+      context.eval(string)
+      context.eval("a.run();")
+    end
+    def default_options
+    js_code = "Agent.prototype.run = function(){ var pd = JSON.stringify({memory: this.memory, events: this.events, options: this.options});create_event(pd); }"
+    {
+      code: js_code
+    }
     end
   end
 end
