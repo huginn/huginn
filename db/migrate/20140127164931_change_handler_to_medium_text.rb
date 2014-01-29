6a11481dea2bf4fe417090a749d7818ac06cb1b5
Alex Piggott <apiggott@ikanow.com>
2014-01-29 17:23:37 -0500

    #141 change delayed_job.handler to mediumtext (16MB) like event.payload
    This isn't a complete fix because if the event.payload is (say) exactly
    16MB then the handler will be >16MB and the same problem will occur.
    I'll push this in for now though, in practice it means it's much less
    likely to occur.

---
+++ db/migrate/20140127164931_change_handler_to_medium_text.rb
@@ -0,0 +1,19 @@
+# Increase handler size to 16MB (consistent with events.payload)
+
+class ChangeHandlerToMediumText < ActiveRecord::Migration
+  def up
+    if mysql?
+      change_column :delayed_jobs, :handler, :text, :limit => 16777215
+    end
+  end
+
+  def down
+    if mysql?
+      change_column :delayed_jobs, :handler, :text, :limit => 65535
+    end
+  end
+
+  def mysql?
+    ActiveRecord::Base.connection.adapter_name =~ /mysql/i
+  end
+end
