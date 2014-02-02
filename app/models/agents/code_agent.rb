c2e27043069c8372ae61628ff9cc731e1fb54f66
Umar M. Sheikh <umar.m.sheikh@gmail.com>
2014-02-02 15:39:53 +0500

    provide both a check method and a receive method in js

--- app/models/agents/code_agent.rb
+++ app/models/agents/code_agent.rb
@@ -6,7 +6,7 @@ module Agents
     #cannot_be_scheduled!
     description <<-MD
       Here is an agent that gives you the ability to specify your own code. We have already provided you
-      a javascript object that has read access to this agent's memory, events, options and the attributes of the agent.
+      a javascript object that has read and write access to this agent's memory, and read access to events, options and the attributes of the agent.
       We also provide you with a method to create events on the server.
       You will be provided with an instance of the Agent object in javascript, with access to the above data.
       You can create events based on your own logic.
@@ -24,16 +24,26 @@ module Agents
             //var new_event = JSON.stringify({key1: "val1", key2: "val2"});
             //create_event(pd);
           }
-      You need to provide the code for the Agent::run function. You code will override any methods already present in the Agent if it has to, and you can use other methods as well with access to the agent properties. You need to at least provide the implementation of Agent.prototype.run so that it can be called periodically, or it can execute when an event happens.
+          Agent.prototype.check = function(){
+            // Implement me
+           }
+          Agent.prototype.receive = function(){
+            // Implement me
+          }
+      You need to provide the code for the Agent::check and Agent::receive function. You code will override any methods already present in the Agent if it has to, and you can use other methods as well with access to the agent properties. You need to at least provide the implementation of Agent.prototype.check and Agent.prototype.receive so that it can be called periodically, or it can execute when an event happens.
 
       We will yield control to your implementation in the following way:
 
           context.eval(js_code); //this is the code that declares the class Agent, and provides a global create_event method.
           context.eval("a = new Agent(events, options, agent)")
           context.eval(options['code'])
-          context.eval("a.run();")
+          If you agent fires periodically {
+          context.eval("a.check();")
+          } else if your agent responds when an event happens {
+            context.eval("a.receive();")
+          }
 
-      You need to provide the run() implementation, as well as other methods it may need to interact with.
+      If your agent responds to events it receive, you need to implement the receive() method, and if you agent fires periodically, you need to implement check(). If your agent does both, please implement both!
 
     MD
     def example_js
@@ -54,6 +64,10 @@ module Agents
     }
     Agent.prototype.run = function(){
     }
+    Agent.prototype.check = function(){
+    }
+    Agent.prototype.receive = function(){
+    }
     H
     end
 
@@ -68,7 +82,8 @@ module Agents
       true
     end
 
-    def execute_js(incoming_events)
+    def execute_js(incoming_events, js_function = "check")
+      js_function = (js_function == "check" ? "check" : "receive")
       context = V8::Context.new
       context.eval(example_js)
       context["create_event"] = lambda {|x,y| puts x; puts y; create_event payload: JSON.parse(y)}
@@ -78,7 +93,8 @@ module Agents
       a, e, o = [self.attributes.to_json, incoming_events.to_json, self.options.to_json]
       string = "a = new Agent('#{e}','#{o}','#{a}');"
       context.eval(string)
-      context.eval("a.run();")
+      runner = "a.#{js_function}();"
+      context.eval(runner)
     end
 
     def check
@@ -90,7 +106,7 @@ module Agents
     end
 
     def default_options
-      js_code = "Agent.prototype.run = function(){ var pd = JSON.stringify({memory: this.memory(), events: this.events, options: this.options});create_event(pd); }"
+      js_code = "Agent.prototype.run = function(){ var pd = JSON.stringify({memory: this.memory(), events: this.events, options: this.options});create_event(pd); };Agent.prototype.check = function(){ var pd = JSON.stringify({memory: this.memory(), events: this.events, options: this.options});create_event(pd); };Agent.prototype.receive = function(){ var pd = JSON.stringify({memory: this.memory(), events: this.events, options: this.options});create_event(pd); }"
       {
         "code" => js_code,
         'expected_receive_period_in_days' => "2"
