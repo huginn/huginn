5f71567a3b1a86c0dda7b3bd1a6fb8c159214c73
Akinori MUSHA <knu@idaemons.org>
2014-04-16 21:08:02 +0900

    Add a setting for using locally installed Graphviz for generating diagrams.
    The Google Charts service is only capable of rendering a limited range
    of characters, so if you name your agents in an exotic language
    generated diagrams are virtually useless being unable to make out.
    
    With this change, Huginn embeds a locally generated SVG diagram instead
    of relying on a third-party API if ENV['USE_GRAPHVIZ_DOT'] is set to a
    command name/path of dot(1).
    
    While at it:
    - Use URI to compose a URL instead of hand-crafting it.
    - Make the dot renderer a helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,19 @@ module ApplicationHelper
       link_to '<span class="label label-warning">No</span>'.html_safe, agent_path(agent, :tab => (agent.recent_error_logs? ? 'logs' : 'details'))
     end
   end
+
+  def render_dot(dot_format_string)
+    if (command = ENV['USE_GRAPHVIZ_DOT']) &&
+       (svg = IO.popen([command, *%w[-Tsvg -q1 -o/dev/stdout /dev/stdin]], 'w+') { |dot|
+          dot.print dot_format_string
+          dot.close_write
+          dot.read
+        } rescue false)
+      svg.html_safe
+    else
+      tag('img', src: URI('https://chart.googleapis.com/chart').tap { |uri|
+            uri.query = URI.encode_www_form(cht: 'gv', chl: dot_format_string)
+          })
+    end
+  end
 end
