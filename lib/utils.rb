13038ce84143c3d44155219dc9d830cb05cdbf4b
Andrew Cantino <cantino@gmail.com>
2014-04-18 00:25:31 -0700

    add ALLOW_JSONPATH_EVAL option to .env

--- lib/utils.rb
+++ lib/utils.rb
@@ -56,7 +56,7 @@ module Utils
       escape = false
     end
 
-    result = JsonPath.new(path, :allow_eval => false).on(data.is_a?(String) ? data : data.to_json)
+    result = JsonPath.new(path, :allow_eval => ENV['ALLOW_JSONPATH_EVAL'] == "true").on(data.is_a?(String) ? data : data.to_json)
     if escape
       result.map {|r| CGI::escape r }
     else
