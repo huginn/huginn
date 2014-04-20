2fa1243dd466b18f229c78cbbb1cdf68dd58d8cf
ZirconCode <gruenings@gmail.com>
2014-04-20 18:51:19 +0200

    Rails 4 deprecated find_by fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-user = User.find_or_initialize_by_email("admin@example.com")
+user = User.find_or_initialize_by(:email => "admin@example.com")
 user.username = "admin"
 user.password = "password"
 user.password_confirmation = "password"
