38818682714b0bfbe5f470e3951fb286932f73d7
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-09 01:25:28 +0930

    Add system install, fix name

--- deployment/site-cookbooks/huginn/recipes/ruby.rb
+++ deployment/site-cookbooks/huginn/recipes/ruby.rb
@@ -1,8 +1,6 @@
-include_recipe "rbenv::default"
-include_recipe "rbenv::ruby_build"
+include_recipe "rbenv::system"
 
 rbenv_ruby node["rbenv"]["ruby_version"]
-
 
 rbenv_gem "rake"
 rbenv_gem "bundle"
@@ -10,7 +8,7 @@ rbenv_gem "bundle"
 
 case node['platform']
 when "centos"
-  packages = %w(libxslt-devel libxml2-devel libmysqlclient-devel curl libffi-devel libssl-devel)
+  packages = %w(libxslt-devel libxml2-devel mysql-devel curl libffi-devel libssl-devel)
 else
   packages = %w(libxslt-dev libxml2-dev curl libmysqlclient-dev libffi-dev libssl-dev)
 end
