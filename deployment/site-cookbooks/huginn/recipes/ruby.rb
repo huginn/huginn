4f7b5b8fab98e8ae105b7f1f6a78e35cc9e10cd3
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-09 11:54:08 +0930

    Manually install plugin

--- deployment/site-cookbooks/huginn/recipes/ruby.rb
+++ deployment/site-cookbooks/huginn/recipes/ruby.rb
@@ -12,6 +12,15 @@ end
 
 rbenv_global node["rbenv"]["ruby_version"]
 
+# TODO Work out why rbenv_plugin doesn't work :/
+directory "#{node[:rbenv][:root_path]}/plugins"
+
+git "#{node[:rbenv][:root_path]}/plugins/rbenv-sudo" do
+  repository 'git://github.com/dcarley/rbenv-sudo.git'
+  action :sync
+end
+
+
 case node['platform']
 when "centos"
   packages = %w(libxslt-devel libxml2-devel mysql-devel curl libffi-devel openssl-devel)
