ffeaaf2d04cc7fa455a0771b7b12d62ac00a7443
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-13 10:57:28 +0930

    Improve documentation

--- app/models/agents/google_calendar_publish_agent.rb
+++ app/models/agents/google_calendar_publish_agent.rb
@@ -7,7 +7,33 @@ module Agents
     description <<-MD
       The GoogleCalendarPublishAgent creates events on your google calendar.
 
+      This agent relies on service accounts, rather than oauth.
 
+      Setup:
+
+      1) Visit https://code.google.com/apis/console/b/0/
+
+      2) New project -> Huginn
+ 
+      3) APIs & Auth -> Enable google calendar
+
+      4) Credentials -> Create new Client ID -> Service Account
+
+      5) Persist the generated private key to a path, ie: /home/hugin/a822ccdefac89fac6330f95039c492dfa3ce6843.p12
+
+      6) Grant access via google calendar UI to the service account email address for each calendar you wish to manage. For a whole google apps domain, you can delegate authority (https://developers.google.com/+/domains/authentication/delegation)
+
+
+      Agent Configuration:
+
+      `calendar_id` - The id the calendar you want to publish to. Typically your google account email address.
+
+      `service_account_email` - The authorised service account.
+
+      `key_file` - The path to the key file.
+
+      `key_secret` - The secret for the key, typically 'notasecret'
+      
 
       Set `expected_update_period_in_days` to the maximum amount of time that you'd expect to pass between Events being created by this Agent.
     MD
@@ -23,8 +49,13 @@ module Agents
     def default_options
       {
         'expected_update_period_in_days' => "10",
-        'calendar_id' => '?',
-        'message' => "{{text}}"
+        'calendar_id' => 'you@email.com',
+        'message' => "{{text}}",
+        'google' => {
+          'key_file' => '/path/to/private.key'
+          'key_secret' => 'notasecret',
+          'service_account_email' => ''
+        }
       }
     end
 
