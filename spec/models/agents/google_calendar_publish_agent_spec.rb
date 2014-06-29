2b38047d47aa6f3d015bf5e4a2e8c3ebf65758ac
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-06-29 22:18:05 +0930

    Tabs -> 2 space soft tab

--- spec/models/agents/google_calendar_publish_agent_spec.rb
+++ spec/models/agents/google_calendar_publish_agent_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Agents::GoogleCalendarPublishAgent, :vcr do
-	before do
-		@valid_params = {
+  before do
+    @valid_params = {
         'expected_update_period_in_days' => "10",
         'calendar_id' => 'sqv39gj35tc837gdns1g4d81cg@group.calendar.google.com',
         'google' => {
@@ -11,28 +11,28 @@ describe Agents::GoogleCalendarPublishAgent, :vcr do
           'service_account_email' => '1029936966326-ncjd7776pcspc98hsg82gsb56t3217ef@developer.gserviceaccount.com'
         }
       }
-		@checker = Agents::GoogleCalendarPublishAgent.new(:name => "somename", :options => @valid_params)
-		@checker.user = users(:jane)
-		@checker.save!
-	end
+    @checker = Agents::GoogleCalendarPublishAgent.new(:name => "somename", :options => @valid_params)
+    @checker.user = users(:jane)
+    @checker.save!
+  end
 
   describe '#receive' do
     it 'should publish any payload it receives' do
       event1 = Event.new
       event1.agent = agents(:bob_manual_event_agent)
       event1.payload = {
-      	'message' => { 
-	        'visibility' => 'default',
-	        'summary' => "Awesome event",
-	        'description' => "An example event with text. Pro tip: DateTimes are in RFC3339",
-	        'end' => {
-	          'dateTime' => '2014-10-02T11:00:00-05:00'
-	        },
-	        'start' => {
-	          'dateTime' => '2014-10-02T10:00:00-05:00'
-	        }       
-	      }
-	    }
+        'message' => { 
+          'visibility' => 'default',
+          'summary' => "Awesome event",
+          'description' => "An example event with text. Pro tip: DateTimes are in RFC3339",
+          'end' => {
+            'dateTime' => '2014-10-02T11:00:00-05:00'
+          },
+          'start' => {
+            'dateTime' => '2014-10-02T10:00:00-05:00'
+          }
+        }
+      }
       event1.save!
 
       @checker.receive([event1])
