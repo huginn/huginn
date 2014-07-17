732d3535172b25a41e4b75ffbefaca02235646ad
Andrew Cantino <cantino@gmail.com>
2014-07-16 23:06:48 -0700

    add params to db/seed.rb; provide better heroku login detection

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-user = User.find_or_initialize_by(:email => "admin@example.com")
-user.username = "admin"
-user.password = "password"
-user.password_confirmation = "password"
+user = User.find_or_initialize_by(:email => ENV['SEED_EMAIL'] || "admin@example.com")
+user.username = ENV['SEED_USERNAME'] || "admin"
+user.password = ENV['SEED_PASSWORD'] || "password"
+user.password_confirmation = ENV['SEED_PASSWORD'] || "password"
 user.invitation_code = User::INVITATION_CODES.first
 user.admin = true
 user.save!
