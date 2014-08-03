a846154c7d90778a44513c536433ba0e0c3b005e
Dominik Sander <git@dsander.de>
2014-08-03 18:26:56 +0200

    Import scenarios with agents which need a service

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605032822) do
+ActiveRecord::Schema.define(version: 20140723110551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
