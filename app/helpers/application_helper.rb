a224a0b9089ac5fa5e0e00c0b8c4a49406628662
Akinori MUSHA <knu@idaemons.org>
2014-08-12 18:58:35 +0900

    Use content_tag.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,19 @@
 module ApplicationHelper
   def nav_link(name, path, options = {})
-    (<<-HTML).html_safe
-      <li class='#{(current_page?(path) ? "active" : "")}'>
-        #{link_to name, path}
-      </li>
-    HTML
+    content_tag :li, link_to(name, path), class: current_page?(path) ? 'active' : ''
   end
 
   def yes_no(bool)
-    if bool
-      '<span class="label label-info">Yes</span>'.html_safe
-    else
-      '<span class="label label-default">No</span>'.html_safe
-    end
+    content_tag :span, bool ? 'Yes' : 'No', class: "label #{bool ? 'label-info' : 'label-default' }"
   end
 
   def working(agent)
     if agent.disabled?
-      link_to 'Disabled', agent_path(agent), :class => 'label label-warning'
+      link_to 'Disabled', agent_path(agent), class: 'label label-warning'
     elsif agent.working?
-      '<span class="label label-success">Yes</span>'.html_safe
+      content_tag :span, 'Yes', class: 'label label-success'
     else
-      link_to 'No', agent_path(agent, :tab => (agent.recent_error_logs? ? 'logs' : 'details')), :class => 'label label-danger'
+      link_to 'No', agent_path(agent, tab: (agent.recent_error_logs? ? 'logs' : 'details')), class: 'label label-danger'
     end
   end
 end
