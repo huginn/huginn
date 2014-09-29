05563022c7ae84b8304fc3dd40cab414a6f6830c
Dean Putney <putney.dean@gmail.com>
2014-09-29 00:41:10 -0700

    Removes validation for non-service authentication

--- app/concerns/tumblr_concern.rb
+++ app/concerns/tumblr_concern.rb
@@ -4,17 +4,7 @@ module TumblrConcern
   included do
     include Oauthable
 
-    validate :validate_tumblr_options
     valid_oauth_providers :tumblr
-  end
-
-  def validate_tumblr_options
-    unless tumblr_consumer_key.present? &&
-      tumblr_consumer_secret.present? &&
-      tumblr_oauth_token.present? &&
-      tumblr_oauth_token_secret.present?
-      errors.add(:base, "Tumblr consumer_key, consumer_secret, oauth_token, and oauth_token_secret are required to authenticate with the Tumblr API.  You can provide these as options to this Agent, or as Credentials with the same names, but starting with 'tumblr_'.")
-    end
   end
 
   def tumblr_consumer_key
