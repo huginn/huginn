afbbf8076252f25d8c7c94b4aa31f97fb74021f6
Dean Putney <putney.dean@gmail.com>
2014-09-30 14:15:45 -0700

    Progress on TumblrBlogAgent

--- app/models/agents/tumblr_blog_agent.rb
+++ app/models/agents/tumblr_blog_agent.rb
@@ -52,18 +52,15 @@ module Agents
     def default_options
       {
         'blog_name' => 'mustardhamsters.tumblr.com',
-        'include_reblogs' => 'true',
-        'expected_update_period_in_days' => '2'
+        'expected_update_period_in_days' => '2',
+        'tag' => '',
+        'type' => '',
       }
     end
 
     def validate_options
       errors.add(:base, "blog_name is required") unless options['blog_name'].present?
       errors.add(:base, "expected_update_period_in_days is required") unless options['expected_update_period_in_days'].present?
-
-      if options[:include_reblogs].present? && !%w[true false].include?(options[:include_reblogs])
-        errors.add(:base, "include_reblogs must be a boolean value string (true/false)")
-      end
 
       if options[:starting_at].present?
         Time.parse(options[:starting_at]) rescue errors.add(:base, "Error parsing starting_at")
@@ -78,20 +75,18 @@ module Agents
       end
     end
 
-    def include_reblogs?
-      interpolated[:include_reblogs] != "false"
-    end
+
 
     def check
       since_id = memory['since_id'] || nil
-      opts = {:count => 200, :include_rts => include_reblogs?, :exclude_replies => false, :include_entities => true, :contributor_details => true}
-      opts.merge! :since_id => since_id unless since_id.nil?
 
-      # http://rdoc.info/gems/tumblr/Tumblr/REST/Timelines#user_timeline-instance_method
-      posts = tumblr.user_timeline(interpolated['blog_name'], opts)
+      posts = tumblr.posts(interpolated['blog_name'])
 
-      posts.each do |post|
-        if post.created_at >= starting_at
+      # p posts
+
+      posts["posts"].each do |post|
+        p post
+        if post['date'] >= starting_at
           memory['since_id'] = post.id if !memory['since_id'] || (post.id > memory['since_id'])
 
           create_event :payload => post.attrs
