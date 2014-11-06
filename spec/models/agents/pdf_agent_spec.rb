ea476dce2e0ebeeb13dcc8f1120919d304d3eefb
Chris Eidhof <chris@eidhof.nl>
2014-11-06 18:13:12 +0100

    Added a spec for pdfinfo

---
+++ spec/models/agents/pdf_agent_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Agents::PdfInfoAgent do
+  let(:agent) do
+    _agent = Agents::PdfInfoAgent.new(name: "PDF Info Agent")
+    _agent.user = users(:bob)
+    _agent.sources << agents(:bob_website_agent)
+    _agent.save!
+    _agent
+  end
+
+  describe "#receive" do
+    before do
+
+      @event = Event.new
+      # @event.agent = agents(:website_agent)
+      @event.payload = {
+        'url' => 'http://mypdf.com',
+      }
+    end
+
+    it "should call HyPDF" do
+      expect {
+        stub(agent).open { "data" }
+        mock(HyPDF).pdfinfo('data') { {title: "Huginn"} }
+        agent.receive([@event])
+      }.to change { Event.count }.by(1)
+    end
+  end
+end
