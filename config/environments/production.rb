0c9c08168c5d279d5a96981fee613787125f7787
Johannes Engelke | hybris <johannes.engelke@hybris.com>
2015-03-20 23:01:15 +0100

    Huginn cloudfoudnry compatible

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Huginn::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = ENV['SERVE_STATIC_ASSETS']
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor  = :uglifier
