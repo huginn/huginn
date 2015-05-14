eba19a53777b02282e1cbe445c36dd317ba430c6
Dominik Sander <git@dsander.de>
2015-05-14 14:25:36 +0200

    Background worker load Dotenv if APP_SECRET_TOKEN is not set
    Unified background worker initialization into pre_runner_boot
    
    Set Rails.configuration.cache_classes = true for background workers

--- bin/schedule.rb
+++ bin/schedule.rb
@@ -3,16 +3,6 @@
 # This process is used to maintain Huginn's upkeep behavior, automatically running scheduled Agents and
 # periodically propagating and expiring Events.  It's typically run via foreman and the included Procfile.
 
-Dotenv.load if Rails.env == 'development'
-
-require 'agent_runner'
-
-unless defined?(Rails)
-  puts
-  puts "Please run me with rails runner, for example:"
-  puts "  RAILS_ENV=production bundle exec rails runner bin/schedule.rb"
-  puts
-  exit 1
-end
+require_relative './pre_runner_boot'
 
 AgentRunner.new(only: HuginnScheduler).run
--- bin/twitter_stream.rb
+++ bin/twitter_stream.rb
@@ -4,16 +4,6 @@
 # new or changed TwitterStreamAgents and starts to follow the stream for them.  It is typically run by foreman via
 # the included Procfile.
 
-Dotenv.load if Rails.env == 'development'
-
-require 'agent_runner'
-
-unless defined?(Rails)
-  puts
-  puts "Please run me with rails runner, for example:"
-  puts "  RAILS_ENV=production bundle exec rails runner bin/twitter_stream.rb"
-  puts
-  exit 1
-end
+require_relative './pre_runner_boot'
 
 AgentRunner.new(only: Agents::TwitterStreamAgent).run
