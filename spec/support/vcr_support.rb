a61bcc60cb076cfc360ddb8069e25f612ff38030
Andrew Cantino <cantino@gmail.com>
2015-07-15 16:57:39 -0400

    stub spec that was making real HTTP call

--- spec/support/vcr_support.rb
+++ spec/support/vcr_support.rb
@@ -2,7 +2,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
-  c.allow_http_connections_when_no_cassette = true
+  c.allow_http_connections_when_no_cassette = false
   c.hook_into :webmock
   c.default_cassette_options = { record: :new_episodes}
   c.configure_rspec_metadata!
