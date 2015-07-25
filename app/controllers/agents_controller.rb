fe857bc6f064aabc893bc29e566624fd62a0c959
Akinori MUSHA <knu@idaemons.org>
2015-07-25 19:06:49 +0900

    Make the dry_run action accept an event payload

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -50,7 +50,13 @@ class AgentsController < ApplicationController
     agent.name ||= '(Untitled)'
 
     if agent.valid?
-      results = agent.dry_run!
+      if event_payload = params[:event]
+        dummy_agent = Agent.build_for_type('ManualEventAgent', current_user, name: 'Dry-Runner')
+        dummy_agent.readonly!
+        event = dummy_agent.events.build(user: current_user, payload: event_payload)
+      end
+
+      results = agent.dry_run!(event)
 
       render json: {
         log: results[:log],
