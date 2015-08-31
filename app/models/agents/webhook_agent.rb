e5eed27afa7ca52458b285bf76b5978ee189c01f
Darren Cauthon <darren@cauthon.com>
2015-08-31 13:15:36 -0500

    Use interpolated instead of params, per suggestion from @cantino.

--- app/models/agents/webhook_agent.rb
+++ app/models/agents/webhook_agent.rb
@@ -46,7 +46,7 @@ module Agents
       return ["Not Authorized", 401] unless secret == interpolated['secret']
 
       #check the verbs
-      verbs = (options['verbs'] || 'post').split(/[,;]/).map { |x| x.strip.downcase }
+      verbs = (interpolated['verbs'] || 'post').split(/[,;]/).map { |x| x.strip.downcase }
       return ["Please use #{verbs.join('/').upcase} requests only", 401] unless verbs.include?(method)
 
       [payload_for(params)].flatten.each do |payload|
