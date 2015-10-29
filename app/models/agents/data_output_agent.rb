c03a60ec4873ffe1b9dfc6a990369e382abbc076
Akinori MUSHA <knu@idaemons.org>
2015-10-29 23:59:22 +0900

    DataOutputAgent: Use `template.self` as URL for the feed itself
    This is useful when you serve a feed generated by DataOutputAgent via
    reverse proxy.

--- app/models/agents/data_output_agent.rb
+++ app/models/agents/data_output_agent.rb
@@ -19,7 +19,7 @@ module Agents
 
           * `secrets` - An array of tokens that the requestor must provide for light-weight authentication.
           * `expected_receive_period_in_days` - How often you expect data to be received by this Agent from other Agents.
-          * `template` - A JSON object representing a mapping between item output keys and incoming event values.  Use [Liquid](https://github.com/cantino/huginn/wiki/Formatting-Events-using-Liquid) to format the values.  Values of the `link`, `title`, `description` and `icon` keys will be put into the \\<channel\\> section of RSS output.  The `item` key will be repeated for every Event.  The `pubDate` key for each item will have the creation time of the Event unless given.
+          * `template` - A JSON object representing a mapping between item output keys and incoming event values.  Use [Liquid](https://github.com/cantino/huginn/wiki/Formatting-Events-using-Liquid) to format the values.  Values of the `link`, `title`, `description` and `icon` keys will be put into the \\<channel\\> section of RSS output.  Value of the `self` key will be used as URL for this feed itself, which is useful when you serve it via reverse proxy.  The `item` key will be repeated for every Event.  The `pubDate` key for each item will have the creation time of the Event unless given.
           * `events_to_show` - The number of events to output in RSS or JSON. (default: `40`)
           * `ttl` - A value for the \\<ttl\\> element in RSS output. (default: `60`)
 
@@ -114,11 +114,12 @@ module Agents
     end
 
     def feed_url(options = {})
-      feed_link + Rails.application.routes.url_helpers.
-                  web_requests_path(agent_id: id || ':id',
-                                    user_id: user_id,
-                                    secret: options[:secret],
-                                    format: options[:format])
+      interpolated['template']['self'].presence ||
+        feed_link + Rails.application.routes.url_helpers.
+                    web_requests_path(agent_id: id || ':id',
+                                      user_id: user_id,
+                                      secret: options[:secret],
+                                      format: options[:format])
     end
 
     def feed_icon
