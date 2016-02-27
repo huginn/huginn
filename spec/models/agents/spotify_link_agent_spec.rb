54d53877449bc78721f27167471ff89e54aba4e6
Dennis Johansson <dennis@dnjo.net>
2016-02-27 17:48:49 +0100

    Add Spotify linker tests

---
+++ spec/models/agents/spotify_link_agent_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe Agents::SpotifyLinkAgent do
+  describe '#receive' do
+    it 'adds an artist_link field to the event payload' do
+      agent = Agents::SpotifyLinkAgent.new
+      agent.name = 'Spotify linker'
+      agent.user = users(:bob)
+      agent.save!
+
+      event = Event.new
+      event.agent = agents(:bob_weather_agent)
+      event.payload = { artist_name: 'Test artist' }
+
+      artist_options = {
+        'name' => 'Test artist',
+        'external_urls' => {
+          'spotify' => 'http://open.spotify.com/test_artist'
+        }
+      }
+      artist = RSpotify::Artist.new(artist_options)
+      mock(RSpotify::Artist).search('Test artist') { [artist] }
+
+      agent.receive([event])
+      expect(Event.last.payload['artist_link'])
+        .to(eq('http://open.spotify.com/test_artist'))
+    end
+  end
+end
