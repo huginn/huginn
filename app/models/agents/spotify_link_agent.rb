c7ec46ea32dd6b3d6659a05e11724dcf2ec6c253
Dennis Johansson <dennis@dnjo.net>
2016-03-05 13:54:58 +0100

    Add track search support

--- app/models/agents/spotify_link_agent.rb
+++ app/models/agents/spotify_link_agent.rb
@@ -7,8 +7,17 @@ module Agents
     def receive(incoming_events)
       incoming_events.each do |event|
         payload = event.payload.dup
-        artist_link = get_spotify_link(payload[:artist_name])
-        payload[:artist_link] = artist_link if artist_link
+        if payload[:artist_name] && payload[:track_name]
+          track_link = get_track_link(
+            payload[:artist_name],
+            payload[:track_name]
+          )
+          payload[:track_link] = track_link if track_link
+        elsif payload[:artist_name]
+          artist_link = get_artist_link(payload[:artist_name])
+          payload[:artist_link] = artist_link if artist_link
+        end
+
         create_event(payload: payload)
       end
     end
@@ -19,7 +28,7 @@ module Agents
 
     private
 
-    def get_spotify_link(artist_name)
+    def get_artist_link(artist_name)
       artists = RSpotify::Artist.search(artist_name)
       artists.each do |artist|
         url = artist.external_urls['spotify']
@@ -27,6 +36,16 @@ module Agents
       end
 
       nil
+    end
+
+    def get_track_link(artist_name, track_name)
+      query = "#{track_name} artist:#{artist_name}"
+      tracks = RSpotify::Track.search(query)
+      unless tracks.empty?
+        track = tracks.first
+        url = track.external_urls['spotify']
+        url
+      end
     end
   end
 end
--- spec/models/agents/spotify_link_agent_spec.rb
+++ spec/models/agents/spotify_link_agent_spec.rb
@@ -1,30 +1,58 @@
 require 'rails_helper'
 
 describe Agents::SpotifyLinkAgent do
+  before(:each) do
+    @agent = Agents::SpotifyLinkAgent.new
+    @agent.name = 'Spotify linker'
+    @agent.user = users(:bob)
+    @agent.save!
+
+    @event = Event.new
+    @event.agent = agents(:bob_weather_agent)
+  end
+
   describe '#receive' do
-    it 'adds an artist_link field to the event payload' do
-      agent = Agents::SpotifyLinkAgent.new
-      agent.name = 'Spotify linker'
-      agent.user = users(:bob)
-      agent.save!
+    context 'when the event payload contains artist_name' do
+      it 'adds an artist_link field to the event payload' do
+        @event.payload = { artist_name: 'Test artist' }
 
-      event = Event.new
-      event.agent = agents(:bob_weather_agent)
-      event.payload = { artist_name: 'Test artist' }
-
-      artist_options = {
-        'name' => 'Test artist',
-        'external_urls' => {
-          'spotify' => 'http://open.spotify.com/test_artist'
+        artist_options = {
+          'name' => 'Test artist',
+          'external_urls' => {
+            'spotify' => 'http://open.spotify.com/test_artist'
+          }
         }
-      }
-      artist = RSpotify::Artist.new(artist_options)
-      mock(RSpotify::Artist).search('Test artist') { [artist] }
+        artist = RSpotify::Artist.new(artist_options)
+        mock(RSpotify::Artist).search('Test artist') { [artist] }
 
-      agent.receive([event])
-      last_payload = Event.last.payload['artist_link']
-      expect(last_payload)
-        .to(eq('http://open.spotify.com/test_artist'))
+        @agent.receive([@event])
+        last_payload = Event.last.payload['artist_link']
+        expect(last_payload)
+          .to(eq('http://open.spotify.com/test_artist'))
+      end
+    end
+
+    context 'when the event payload contains artist_name and track_name' do
+      it 'adds a track_link to the event payload' do
+        @event.payload = {
+          artist_name: 'Test artist',
+          track_name: 'Test track'
+        }
+
+        track_options = {
+          'name' => 'Test track',
+          'external_urls' => {
+            'spotify' => 'http://open.spotify.com/test_track'
+          }
+        }
+        track = RSpotify::Track.new(track_options)
+        mock(RSpotify::Track).search(anything) { [track] }
+
+        @agent.receive([@event])
+        last_payload = Event.last.payload['track_link']
+        expect(last_payload)
+          .to(eq('http://open.spotify.com/test_track'))
+      end
     end
   end
 end
