43463b5518a7660764d438dcdab367dabd13332a
Dominik Sander <git@dsander.de>
2016-03-13 11:24:17 +0100

    Increase capybara/poltergeist timeout to 60 seconds
    The default wait time of 2 seconds produced false positives on slow test runners.

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 require 'capybara-select2'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, timeout: 60)
+end
+
 Capybara.javascript_driver = :poltergeist
+Capybara.default_max_wait_time = 60
 
 RSpec.configure do |config|
   config.include Warden::Test::Helpers
