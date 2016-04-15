f70e7aae1dddbed9d2e5b07e99f086a9f85238cc
Judy Ngai <judy.ngai1228@gmail.com>
2016-04-15 16:54:25 -0400

    work on upload icon

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -6,7 +6,7 @@ class ScenariosController < ApplicationController
     set_table_sort sorts: %w[name public], default: { name: :asc }
 
     @scenarios = current_user.scenarios.reorder(table_sort).page(params[:page])
-    @glyph = session[:scenario_icon]
+    @glyph = session[:icon]
 
     respond_to do |format|
       format.html
@@ -70,7 +70,7 @@ class ScenariosController < ApplicationController
 
   def create
     @scenario = current_user.scenarios.build(params[:scenario])
-    session[:scenario_icon] = params[:scenario_icon]
+    session[:icon] = params[:scenario_icon]
 
     respond_to do |format|
       if @scenario.save
@@ -85,7 +85,7 @@ class ScenariosController < ApplicationController
 
   def update
     @scenario = current_user.scenarios.find(params[:id])
-    session[:scenario_icon] = params[:scenario_icon]
+    session[:icon] = params[:scenario_icon]
 
     respond_to do |format|
       if @scenario.update_attributes(params[:scenario])
