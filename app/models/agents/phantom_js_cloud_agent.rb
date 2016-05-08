e578cefbad38aea23bd712c2eea6e7fa3a068a0e
Irfan Charania <irfan.charania@gmail.com>
2016-05-08 10:39:19 -0700

    Add options

--- app/models/agents/phantom_js_cloud_agent.rb
+++ app/models/agents/phantom_js_cloud_agent.rb
@@ -6,17 +6,33 @@ include ERB::Util
 module Agents
   class PhantomJsCloudAgent < Agent
     include FormConfigurable
+    include WebRequestConcern
 
     can_dry_run!
     
     default_schedule "every_12h"
     
     description <<-MD
-      The Phantom Js Cloud Agent is used to formulate a url in accordance with the PhantomJs Cloud API.
+      [PhantomJs Cloud](https://phantomjscloud.com/) renders webpages in much the same way as a browser would, 
+      and allows the Website Agent to properly scrape dynamic content from javascript-heavy pages.
+      
+      The Phantom Js Cloud Agent is used to formulate a url in accordance with the [PhantomJs Cloud API](https://phantomjscloud.com/docs/index.html).
+      This url can then be supplied to Website Agent to fetch and parse content.
+      
+      [Sign up](https://dashboard.phantomjscloud.com/dash.html#/signup) to get an api key, and add it in Huginn credentials.
 
-      ** TODO **
+      
+      Options:
 
-      Reference: https://phantomjscloud.com/docs/index.html
+      * `url` - The url to render
+      * `render_type` - Render as html or plain text without html tags (default: `html`)
+      * `output_as_json` - Return the page conents and metadata as a JSON object (default: `false`)
+      * `ignore_images` - Skip loading of inlined images (default: `false`)
+      * `url_agent` - A custom User-Agent name (default: `#{default_user_agent}`)
+      * `wait_interval` - Milliseconds to delay rendering after the last resource is finished loading.
+      This is useful in case there are any AJAX requests or animations that need to finish up. 
+      This can safely be set to 0 if you know there are no AJAX or animations you need to wait for (default: `1000`ms)
+
     MD
 
     event_description <<-MD
@@ -30,47 +46,103 @@ module Agents
       {
           'url' => "http://xkcd.com",
           'render_type' => "html",
-          'user_agent' => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"          
+          'output_as_json' => false,
+          'ignore_images' => false,
+          'user_agent' => self.class.default_user_agent,
+          'wait_interval' => "1000"
       }
     end
 
-    form_configurable :api_key, type: :array, values: ['** TODO ** list of credentials']
+    form_configurable :api_key, roles: :completable
     form_configurable :url        
     form_configurable :render_type, type: :array, values: ['html', 'plainText']
+    form_configurable :output_as_json, type: :boolean
+    form_configurable :ignore_images, type: :boolean
     form_configurable :user_agent, type: :text
+    form_configurable :wait_interval
 
     def renderType
-        interpolated['render_type'].presence || 'html'
+      interpolated['render_type'].presence || default_options.render_type
+    end
+
+    def outputAsJson
+      boolify(interpolated['output_as_json'].presence || default_options.output_as_json)
+    end
+
+    def ignoreImages
+      boolify(interpolated['ignore_images'].presence || default_options.ignore_images)
+    end
+
+    def userAgent
+      interpolated['user_agent'].presence || self.class.default_user_agent
+    end
+
+    def waitInterval
+      interpolated['wait_interval'].presence || default_options.wait_interval
+    end
+
+    def get_page_request_settings()
+      prs = {}
+
+      if ignoreImages
+        prs.merge!(ignoreImages: ignoreImages)
+      end
+
+      if userAgent.present?
+        prs.merge!(userAgent: userAgent)
+      end
+
+      if waitInterval != default_options['wait_interval']
+        prs.merge!(wait_interval: waitInterval)
+      end
+
+      prs
     end
 
     def build_phantom_url(url)
       api_key = interpolated[:api_key]
-      request_hash = {
+      page_request_hash = {
         :url => url,
-        :renderType => renderType,
-        :requestSettings => {
-          :userAgent => interpolated[:user_agent]
-        }
+        :renderType => renderType
       }
 
-      request = request_hash.to_json
-      encoded = url_encode(request)
+      if outputAsJson
+        page_request_hash.merge!(outputAsJson: outputAsJson)
+      end
 
-      phantom_url = "https://phantomjscloud.com/api/browser/v2/#{api_key}/?request=#{encoded}"
-      log "Generated #{phantom_url}"
-      create_event payload: { 'url' => phantom_url }
+
+      page_request_settings_hash = get_page_request_settings()      
+
+      if page_request_settings_hash.any?
+        page_request_hash.merge!(requestSettings: page_request_settings_hash)
+      end
+
+      request = page_request_hash.to_json
+      log "Generated request: #{request}"
+
+      encoded = url_encode(request)
+      "https://phantomjscloud.com/api/browser/v2/#{api_key}/?request=#{encoded}"
     end
 
     def check
-      build_phantom_url (interpolated[:url])
+      phantom_url = build_phantom_url(interpolated[:url])
+
+      create_event payload: { 'url' => phantom_url }
     end
 
     def receive(incoming_events)
       log "receiving..."
       incoming_events.each do |event|
-        mo = interpolated(event)
-        build_phantom_url (mo['url'])
+        interpolate_with(event) do
+          phantom_url = build_phantom_url(interpolated('url'))
+
+          create_event payload: { 'url' => phantom_url }
+        end
       end
+    end
+
+    def complete_api_key
+      user.user_credentials.map { |c| {text: c.credential_name, id: "{% credential #{c.credential_name} %}"} }
     end
 
     def working?
