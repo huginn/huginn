kreuzwerker/DKT.huginn

Ensure to only send supported agent connections
When switching between Agent types in the in the new Agent form we need to clear the select fields values (targets,
receivers, control targets) when the current Agent does not support the respective method.

spec/features/create_an_agent_spec.rb
@@ -56,4 +56,43 @@ describe "Creating a new agent", js: true do
     click_on "SF Weather"
     expect(page).to have_content "Editing your WeatherAgent"
   end
+
+  context "clearing unsupported fields of agents" do
+    before do
+      visit new_agent_path
+    end
+
+    it "does not send previously configured sources when the current agent does not support them" do
+      select2("Website Agent", from: "Type")
+      select2("SF Weather", from: 'Sources')
+      select2("Webhook Agent", from: "Type")
+      fill_in(:agent_name, with: "No sources")
+      click_on "Save"
+      expect(page).to have_content("No sources")
+      agent = Agent.find_by(name: "No sources")
+      expect(agent.sources).to eq([])
+    end
+
+    it "does not send previously configured control targets when the current agent does not support them" do
+      select2("Commander Agent", from: "Type")
+      select2("SF Weather", from: 'Control targets')
+      select2("Webhook Agent", from: "Type")
+      fill_in(:agent_name, with: "No control targets")
+      click_on "Save"
+      expect(page).to have_content("No control targets")
+      agent = Agent.find_by(name: "No control targets")
+      expect(agent.control_targets).to eq([])
+    end
+
+    it "does not send previously configured receivers when the current agent does not support them" do
+      select2("Website Agent", from: "Type")
+      select2("ZKCD", from: 'Targets')
+      select2("Email Agent", from: "Type")
+      fill_in(:agent_name, with: "No receivers")
+      click_on "Save"
+      expect(page).to have_content("No receivers")
+      agent = Agent.find_by(name: "No receivers")
+      expect(agent.receivers).to eq([])
+    end
+  end
 end

huginn/huginn

spec/features/create_an_agent_spec.rb
@@ -56,4 +56,43 @@ describe "Creating a new agent", js: true do
     click_on "SF Weather"
     expect(page).to have_content "Editing your WeatherAgent"
   end
+
+  context "clearing unsupported fields of agents" do
+    before do
+      visit new_agent_path
+    end
+
+    it "does not send previously configured sources when the current agent does not support them" do
+      select2("Website Agent", from: "Type")
+      select2("SF Weather", from: 'Sources')
+      select2("Webhook Agent", from: "Type")
+      fill_in(:agent_name, with: "No sources")
+      click_on "Save"
+      expect(page).to have_content("No sources")
+      agent = Agent.find_by(name: "No sources")
+      expect(agent.sources).to eq([])
+    end
+
+    it "does not send previously configured control targets when the current agent does not support them" do
+      select2("Commander Agent", from: "Type")
+      select2("SF Weather", from: 'Control targets')
+      select2("Webhook Agent", from: "Type")
+      fill_in(:agent_name, with: "No control targets")
+      click_on "Save"
+      expect(page).to have_content("No control targets")
+      agent = Agent.find_by(name: "No control targets")
+      expect(agent.control_targets).to eq([])
+    end
+
+    it "does not send previously configured receivers when the current agent does not support them" do
+      select2("Website Agent", from: "Type")
+      select2("ZKCD", from: 'Receivers')
+      select2("Email Agent", from: "Type")
+      fill_in(:agent_name, with: "No receivers")
+      click_on "Save"
+      expect(page).to have_content("No receivers")
+      agent = Agent.find_by(name: "No receivers")
+      expect(agent.receivers).to eq([])
+    end
+  end
 end