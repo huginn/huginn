a146fc82533e5c6e9a35d2a61070b3a269575755
Judy Ngai <judy.ngai1228@gmail.com>
2016-05-19 13:09:13 -0400

    improved test and refactored method per rubocop warnings

--- app/controllers/scenarios_controller.rb
+++ app/controllers/scenarios_controller.rb
@@ -97,15 +97,11 @@ class ScenariosController < ApplicationController
 
   def enable_or_disable_all_agents
     @scenario = current_user.scenarios.find(params[:id])
+    @scenario.agents.update_all disabled: params[:scenario][:disabled]
 
     respond_to do |format|
-      if @scenario.agents.update_all disabled: params[:scenario][:disabled]
-        format.html { redirect_to @scenario, notice: 'The agents in this scenario has been successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @scenario.errors, status: :unprocessable_entity }
-      end
+      format.html { redirect_to @scenario }
+      flash[:notice] = 'The agents has been successfully updated.'
     end
   end
 
--- spec/controllers/scenarios_controller_spec.rb
+++ spec/controllers/scenarios_controller_spec.rb
@@ -140,10 +140,12 @@ describe ScenariosController do
     end
   end
 
-  describe "PUT enable_or_disable_all_agents" do
-    it "updates disabled on all agents in a scenario for the current user" do
-      put :enable_or_disable_all_agents, :id => scenarios(:bob_weather), :scenario => { "scenario"=>{"disabled"=>"0"}}
-      expect(scenarios(:bob_weather).agents.where(:name => "Disabled Agent")[0].disabled).to eq(false)
+  describe 'PUT enable_or_disable_all_agents' do
+    it 'updates disabled on all agents in a scenario for the current user' do
+      @scenario = { 'scenario' => { 'disabled' => '0' } }
+      @id = scenarios(:bob_weather).id
+      put :enable_or_disable_all_agents, id: @id, scenario: @scenario
+      expect(agents(:bob_disabled_website_agent).disabled).to eq(false)
       expect(response).to redirect_to(scenario_path(scenarios(:bob_weather)))
     end
   end
