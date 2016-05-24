4a08f92b1dd3c6acd1cff8989d39bdf5f876ac81
Alex Jordan <alex@strugee.net>
2016-05-24 12:49:40 -0700

    Add WIP support for HTTP headers

--- app/models/agents/http_status_agent.rb
+++ app/models/agents/http_status_agent.rb
@@ -14,9 +14,9 @@ module Agents
     form_configurable :disable_redirect_follow, type: :array, values: ['true', 'false']
 
     description <<-MD
-      The HttpStatusAgent will check a url and emit the resulting HTTP status code with the time that it waited for a reply.
+      The HttpStatusAgent will check a url and emit the resulting HTTP status code with the time that it waited for a reply. Additionally, it will optionally emit the value of a specified header.
 
-      Specify a `Url` and the Http Status Agent will produce an event with the http status code.
+      Specify a `Url` and the Http Status Agent will produce an event with the HTTP status code. If you specify a `Header` as well, that header's value will be included in the event.
 
       The `disable redirect follow` option causes the Agent to not follow HTTP redirects. For example, setting this to `true` will cause an agent that receives a 301 redirect to `http://yahoo.com` to return a status of 301 instead of following the redirect and returning 200.
     MD
@@ -26,8 +26,13 @@ module Agents
 
           {
             "url": "...",
+<<<<<<< HEAD:app/models/agents/http_status_agent.rb
             "status": "..."
             "elapsed_time": "..."
+=======
+            "status": "...",
+            "header": "..."
+>>>>>>> 94ca7f3... Add WIP support for HTTP headers:app/models/agents/http_agent.rb
           }
     MD
 
@@ -47,24 +52,34 @@ module Agents
     end
 
     def check
-      check_this_url interpolated[:url]
+      check_this_url interpolated[:url] false
     end
 
     def receive(incoming_events)
       incoming_events.each do |event|
         interpolate_with(event) do
-          check_this_url interpolated[:url]
+          check_this_url interpolated[:url] false
         end
       end
     end
 
     private
 
+<<<<<<< HEAD:app/models/agents/http_status_agent.rb
     def check_this_url(url)
       measured_result = TimeTracker.track { ping(url) }
       if measured_result.result
         create_event payload: { 'url' => url, 'status' => measured_result.status.to_s, 'response_received' => true, 'elapsed_time' => measured_result.elapsed_time }
         memory['last_status'] = measured_result.status.to_s
+=======
+    def check_this_url(url, header)
+      if result = ping(url)
+        if header?
+          create_event payload: { 'url' => url, 'status' => result.status.to_s, 'header' => header, 'header_value' => result.headers[header], 'response_received' => true }
+        else
+          create_event payload: { 'url' => url, 'status' => result.status.to_s, 'response_received' => true }
+        memory['last_status'] = result.status.to_s
+>>>>>>> 94ca7f3... Add WIP support for HTTP headers:app/models/agents/http_agent.rb
       else
         create_event payload: { 'url' => url, 'response_received' => false, 'elapsed_time' => measured_result.elapsed_time }
         memory['last_status'] = nil
