bfd637c3dab02a6669de7b419952c867d24bc83d
Alex Jordan <alex@strugee.net>
2016-06-15 17:52:49 -0700

    Add multiple header support

--- app/models/agents/http_status_agent.rb
+++ app/models/agents/http_status_agent.rb
@@ -12,7 +12,7 @@ module Agents
 
     form_configurable :url
     form_configurable :disable_redirect_follow, type: :array, values: ['true', 'false']
-    form_configurable :header
+    form_configurable :headers_to_save
 
     description <<-MD
       The HttpStatusAgent will check a url and emit the resulting HTTP status code with the time that it waited for a reply. Additionally, it will optionally emit the value of one or more specified headers.
@@ -49,21 +49,25 @@ module Agents
       errors.add(:base, "a url must be specified") unless options['url'].present?
     end
 
+    def header_array(str)
+      str.split(',').map { |el| el.strip }
+    end
+
     def check
-      check_this_url interpolated[:url], interpolated[:headers]
+      check_this_url interpolated[:url], header_array(interpolated[:headers_to_save])
     end
 
     def receive(incoming_events)
       incoming_events.each do |event|
         interpolate_with(event) do
-          check_this_url interpolated[:url], interpolated[:headers]
+          check_this_url interpolated[:url], header_array(interpolated[:headers_to_save])
         end
       end
     end
 
     private
 
-    def check_this_url(url, headers)
+    def check_this_url(url, local_headers)
       # Track time
       measured_result = TimeTracker.track { ping(url) }
 
@@ -73,8 +77,11 @@ module Agents
       if measured_result.result
         payload.merge!({ 'response_received' => true, 'status' => measured_result.status.to_s })
         # Deal with headers
-        if headers.present?
-          payload.merge!({ 'headers' => headers, 'header_values' => measured_result.result.headers[headers] })
+        if local_headers.present?
+          header_results = measured_result.result.headers.select {|header, value| local_headers.include?(header)}
+          # Fill in headers that we wanted, but weren't returned
+          local_headers.each { |header| header_results[header] = nil unless header_results.has_key?(header) }
+          payload.merge!({ 'headers' => header_results })
         end
         create_event payload: payload
         memory['last_status'] = measured_result.status.to_s
