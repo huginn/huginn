kreuzwerker/DKT.huginn

Allow specifing Agent gem in the Gemfile

Gemfile
@@ -193,3 +193,7 @@ end
 if_true(ENV['DATABASE_ADAPTER'].strip == 'mysql2') do
   gem 'mysql2', '~> 0.3.20'
 end
+
+GemfileHelper.parse_agent_gems(ENV['ADDITIOANL_GEMS']) do |args|
+  gem *args
+end

lib/gemfile_helper.rb
@@ -19,7 +19,32 @@ class GemfileHelper
                               )
     end
 
+    GEM_NAME = '[A-Za-z0-9\.\-\_]+'.freeze
+    GEM_OPTIONS = '(.+?)\s*(?:,\s*(.+?)){0,1}'.freeze
+    GEM_SEPARATOR = '\s*(?:,|\z)'.freeze
+    GEM_REGULAR_EXPRESSION = /(#{GEM_NAME})(?:\(#{GEM_OPTIONS}\)){0,1}#{GEM_SEPARATOR}/
+
+    def parse_agent_gems(string)
+      return unless string
+      string.scan(GEM_REGULAR_EXPRESSION).each do |name, version, args|
+        if version =~ /\w+:/
+          args = "#{version},#{args}"
+          version = nil
+        end
+        yield [name, version, parse_gem_args(args)].compact
+      end
+    end
+
     private
+
+    def parse_gem_args(args)
+      return nil unless args
+      options = {}
+      args.scan(/(\w+):\s*(.+?)#{GEM_SEPARATOR}/).each do |key, value|
+        options[key.to_sym] = value
+      end
+      options
+    end
 
     def sanity_check(env)
       return if ENV['CI'] == 'true' || !env.empty?

spec/lib/gemfile_helper_spec.rb
@@ -1,0 +1,48 @@
+require 'rails_helper'
+
+describe GemfileHelper do
+  context 'parse_agent_gems' do
+    VALID_STRINGS = [
+      ['huginn_nlp_agents(~> 0.2.1)', [
+        ['huginn_nlp_agents', '~> 0.2.1']
+      ]],
+      ['huginn_nlp_agents(~> 0.2.1, git: http://github.com/dsander/huginn.git, branch: agents_in_gems)',
+        [['huginn_nlp_agents', '~> 0.2.1', git: 'http://github.com/dsander/huginn.git', branch: 'agents_in_gems']]
+      ],
+      ['huginn_nlp_agents(~> 0.2.1, git: http://github.com/dsander/huginn.git, ref: 2342asdab)  , huginn_nlp_agents(~> 0.2.1)', [
+        ['huginn_nlp_agents', '~> 0.2.1', git: 'http://github.com/dsander/huginn.git', ref: '2342asdab'],
+        ['huginn_nlp_agents', '~> 0.2.1']
+      ]],
+      ['huginn_nlp_agents(~> 0.2.1, path: /tmp/test)', [
+        ['huginn_nlp_agents', '~> 0.2.1', path: '/tmp/test']
+      ]],
+      ['huginn_nlp_agents', [
+        ['huginn_nlp_agents']
+      ]],
+      ['huginn_nlp_agents, test(0.1), test2(github: test2/huginn_test)', [
+        ['huginn_nlp_agents'],
+        ['test', '0.1'],
+        ['test2', github: 'test2/huginn_test']
+      ]],
+      ['huginn_nlp_agents(git: http://github.com/dsander/huginn.git, ref: 2342asdab)', [
+        ['huginn_nlp_agents', git: 'http://github.com/dsander/huginn.git', ref: '2342asdab']
+      ]],
+    ]
+
+    it 'parses valid gem strings correctly' do
+      VALID_STRINGS.each do |string, outcomes|
+        GemfileHelper.parse_agent_gems(string) do |args|
+          expect(args).to eq(outcomes.shift)
+        end
+      end
+    end
+
+    it 'does nothing when nil is passed' do
+      expect { |b| GemfileHelper.parse_agent_gems(nil, &b) }.not_to yield_control
+    end
+
+    it 'does nothing when an empty string is passed' do
+      expect { |b| GemfileHelper.parse_agent_gems('', &b) }.not_to yield_control
+    end
+  end
+end

huginn/huginn

Gemfile
@@ -195,3 +195,7 @@ end
 if_true(ENV['DATABASE_ADAPTER'].strip == 'mysql2') do
   gem 'mysql2', '~> 0.3.20'
 end
+
+GemfileHelper.parse_each_agent_gem(ENV['ADDITIONAL_GEMS']) do |args|
+  gem *args
+end

lib/gemfile_helper.rb
@@ -19,7 +19,32 @@ class GemfileHelper
                               )
     end
 
+    GEM_NAME = '[A-Za-z0-9\.\-\_]+'.freeze
+    GEM_OPTIONS = '(.+?)\s*(?:,\s*(.+?)){0,1}'.freeze
+    GEM_SEPARATOR = '\s*(?:,|\z)'.freeze
+    GEM_REGULAR_EXPRESSION = /(#{GEM_NAME})(?:\(#{GEM_OPTIONS}\)){0,1}#{GEM_SEPARATOR}/
+
+    def parse_each_agent_gem(string)
+      return unless string
+      string.scan(GEM_REGULAR_EXPRESSION).each do |name, version, args|
+        if version =~ /\w+:/
+          args = "#{version},#{args}"
+          version = nil
+        end
+        yield [name, version, parse_gem_args(args)].compact
+      end
+    end
+
     private
+
+    def parse_gem_args(args)
+      return nil unless args
+      options = {}
+      args.scan(/(\w+):\s*(.+?)#{GEM_SEPARATOR}/).each do |key, value|
+        options[key.to_sym] = value
+      end
+      options
+    end
 
     def sanity_check(env)
       return if ENV['CI'] == 'true' || !env.empty?

spec/lib/gemfile_helper_spec.rb
@@ -1,0 +1,48 @@
+require 'rails_helper'
+
+describe GemfileHelper do
+  context 'parse_each_agent_gem' do
+    VALID_STRINGS = [
+      ['huginn_nlp_agents(~> 0.2.1)', [
+        ['huginn_nlp_agents', '~> 0.2.1']
+      ]],
+      ['huginn_nlp_agents(~> 0.2.1, git: http://github.com/dsander/huginn.git, branch: agents_in_gems)',
+        [['huginn_nlp_agents', '~> 0.2.1', git: 'http://github.com/dsander/huginn.git', branch: 'agents_in_gems']]
+      ],
+      ['huginn_nlp_agents(~> 0.2.1, git: http://github.com/dsander/huginn.git, ref: 2342asdab)  , huginn_nlp_agents(~> 0.2.1)', [
+        ['huginn_nlp_agents', '~> 0.2.1', git: 'http://github.com/dsander/huginn.git', ref: '2342asdab'],
+        ['huginn_nlp_agents', '~> 0.2.1']
+      ]],
+      ['huginn_nlp_agents(~> 0.2.1, path: /tmp/test)', [
+        ['huginn_nlp_agents', '~> 0.2.1', path: '/tmp/test']
+      ]],
+      ['huginn_nlp_agents', [
+        ['huginn_nlp_agents']
+      ]],
+      ['huginn_nlp_agents, test(0.1), test2(github: test2/huginn_test)', [
+        ['huginn_nlp_agents'],
+        ['test', '0.1'],
+        ['test2', github: 'test2/huginn_test']
+      ]],
+      ['huginn_nlp_agents(git: http://github.com/dsander/huginn.git, ref: 2342asdab)', [
+        ['huginn_nlp_agents', git: 'http://github.com/dsander/huginn.git', ref: '2342asdab']
+      ]],
+    ]
+
+    it 'parses valid gem strings correctly' do
+      VALID_STRINGS.each do |string, outcomes|
+        GemfileHelper.parse_each_agent_gem(string) do |args|
+          expect(args).to eq(outcomes.shift)
+        end
+      end
+    end
+
+    it 'does nothing when nil is passed' do
+      expect { |b| GemfileHelper.parse_each_agent_gem(nil, &b) }.not_to yield_control
+    end
+
+    it 'does nothing when an empty string is passed' do
+      expect { |b| GemfileHelper.parse_each_agent_gem('', &b) }.not_to yield_control
+    end
+  end
+end