2c7ffb43f2cb302c9dbb334ec0ccf6606b8ce74f
Darren Cauthon <darren@cauthon.com>
2016-07-19 06:29:40 -0500

    Refactor for better testing.

--- spec/models/agents/liquid_output_agent_spec.rb
+++ spec/models/agents/liquid_output_agent_spec.rb
@@ -63,13 +63,22 @@ describe Agents::LiquidOutputAgent do
   end
 
   describe "#receive?" do
+
+    let(:key)   { SecureRandom.uuid }
+    let(:value) { SecureRandom.uuid }
+
+    let(:incoming_events) do
+      last_payload = { key => value }
+      [Struct.new(:payload).new( { key => SecureRandom.uuid } ),
+       Struct.new(:payload).new( { key => SecureRandom.uuid } ),
+       Struct.new(:payload).new(last_payload)]
+    end
+
     it "stores the last event in memory" do
-      key, value = SecureRandom.uuid, SecureRandom.uuid
-      last_payload = { key => value}
-      incoming_events = [Object.new, Object.new, Struct.new(:payload).new(last_payload)]
       agent.receive incoming_events
       expect(agent.memory['last_event'][key]).to equal(value)
     end
+
   end
 
   describe "#receive_web_request?" do
