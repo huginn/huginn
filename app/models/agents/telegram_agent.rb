990373563104778a7f8968d661630ffd5534ad11
colson <callen.olson@gmail.com>
2016-11-14 22:06:06 -0800

    Revert "chunk text into multiple Telegrams to send"
    This reverts commit 9e45d9fd884274ef263461933ff58249687650c0.

--- app/models/agents/telegram_agent.rb
+++ app/models/agents/telegram_agent.rb
@@ -32,8 +32,8 @@ module Agents
 
     def default_options
       {
-        auth_token: 'xxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-        chat_id: 'xxxxxxxxxxxxxxxx'
+        auth_token: 'xxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        chat_id: 'xxxxxxxx'
       }
     end
 
@@ -79,12 +79,7 @@ module Agents
     def send_telegram_message(method, params)
       params[:chat_id] = interpolated['chat_id']
       params[:parse_mode] = interpolated['parse_mode'] if interpolated['parse_mode'].present?
-
-      txt_message = params[:text].scan /.{1,4096}\W/m
-      txt_message.each do | message |
-        params[:text] = message
-        HTTMultiParty.post telegram_bot_uri(method), query: params
-      end
+      HTTMultiParty.post telegram_bot_uri(method), query: params
     end
 
     def load_field(event, field)
