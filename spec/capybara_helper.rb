e5776f206e4da3e1635ee56423df0c77223341c8
Andrew Cantino <cantino@users.noreply.github.com>
2017-02-27 11:25:24 -0800

    Make feature specs more robust (#1917)

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -15,6 +15,7 @@ Capybara.default_max_wait_time = CAPYBARA_TIMEOUT
 RSpec.configure do |config|
   config.include Warden::Test::Helpers
   config.include AlertConfirmer, type: :feature
+  config.include FeatureHelpers, type: :feature
 
   config.before(:suite) do
     Warden.test_mode!
