424d04bd144577be0261e1d866db6a1e8d6600cf
joenas <jon@jonnev.se>
2017-04-25 11:40:28 +0200

    PR fixes: No more hash rockets Added info to description Indentation

--- app/models/agents/google_translation_agent.rb
+++ app/models/agents/google_translation_agent.rb
@@ -7,8 +7,15 @@ module Agents
     description <<-MD
       The Translation Agent will attempt to translate text between natural languages.
 
-      Services are provided using Google Translate. You can [sign up](https://cloud.google.com/translate/) to get `GOOGLE_API_KEY` which is required to use this agent.
+      Services are provided using Google Translate. You can [sign up](https://cloud.google.com/translate/) to get `google_api_key` which is required to use this agent.
 
+      `to` must be filled with a [translator language code](https://cloud.google.com/translate/docs/languages).
+
+      `from` is the language translated from. If it's not specified, the API will attempt to detect the source language automatically and return it within the response.
+
+      Specify what you would like to translate in `content` field, you can use [Liquid](https://github.com/cantino/huginn/wiki/Formatting-Events-using-Liquid) specify which part of the payload you want to translate.
+
+      `expected_receive_period_in_days` is the maximum number of days you would allow to pass between events.
     MD
 
     event_description "User defined"
@@ -17,10 +24,11 @@ module Agents
       {
         'to' => "sv",
         'from' => 'en',
+        'google_api_key' => '',
         'expected_receive_period_in_days' => 1,
         'content' => {
-          'text' => "{{message.text}}",
-          'content' => "{{xyz}}"
+          'text' => "{{message}}",
+          'moretext' => "{{another message}}"
         }
       }
     end
@@ -30,8 +38,8 @@ module Agents
     end
 
     def validate_options
-      unless ENV['GOOGLE_API_KEY'].present? && options['to'].present? && options['content'].present? && options['expected_receive_period_in_days'].present?
-        errors.add :base, "GOOGLE_API_KEY, to,expected_receive_period_in_days and content are all required"
+      unless options['google_api_key'].present? && options['to'].present? && options['content'].present? && options['expected_receive_period_in_days'].present?
+        errors.add :base, "google_api_key, to, content and expected_receive_period_in_days are all required"
       end
     end
 
@@ -40,17 +48,19 @@ module Agents
     end
 
     def receive(incoming_events)
-      translate = Google::Apis::TranslateV2::TranslateService.new
-      translate.key = ENV['GOOGLE_API_KEY']
       incoming_events.each do |event|
         translated_event = {}
         opts = interpolated(event)
         opts['content'].each_pair do |key, value|
-          result = translate.list_translations(value, opts['to'], source: translate_from)
+          result = translate_service.list_translations(value, opts['to'], source: translate_from)
           translated_event[key] = result.translations.last.translated_text
         end
-        create_event :payload => translated_event
+        create_event payload: translated_event
       end
+    end
+
+    def translate_service
+      @translate_service ||= Google::Apis::TranslateV2::TranslateService.new.tap {|service| service.key = options['google_api_key']}
     end
   end
 end
