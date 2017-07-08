8645323dfeccc9def0fbd3d253a934b253c8d515
Akinori MUSHA <knu@idaemons.org>
2017-07-09 03:24:26 +0900

    Expose `target` to CommanderAgent in templating
    This allows a CommanderAgent to control only some of the targets
    depending on the agent attributes.  For example, you can update the
    threshold value of a target agent only if the current value is
    lower (or higher) than the suggested new value.

--- spec/models/agents/commander_agent_spec.rb
+++ spec/models/agents/commander_agent_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe Agents::CommanderAgent do
+  let(:target) {
+    agents(:bob_website_agent)
+  }
+
   let(:valid_params) {
     {
       name: 'Example',
@@ -8,27 +12,27 @@ describe Agents::CommanderAgent do
       options: {
         'action' => 'run',
       },
+      user: users(:bob),
+      control_targets: [target]
     }
   }
 
   let(:agent) {
-    described_class.create!(valid_params) { |agent|
-      agent.user = users(:bob)
-    }
+    described_class.create!(valid_params)
   }
 
   it_behaves_like AgentControllerConcern
 
   describe "check" do
     it "should command targets" do
-      stub(agent).control!.once { nil }
+      stub(Agent).async_check(target.id).once { nil }
       agent.check
     end
   end
 
   describe "receive_events" do
     it "should command targets" do
-      stub(agent).control!.once { nil }
+      stub(Agent).async_check(target.id).once { nil }
 
       event = Event.new
       event.agent = agents(:bob_rain_notifier_agent)
@@ -37,6 +41,63 @@ describe Agents::CommanderAgent do
         'link' => 'Random',
       }
       agent.receive([event])
+    end
+
+    context "to configure" do
+      let(:real_target) {
+        Agents::TriggerAgent.create!(
+          name: "somename",
+          options: {
+            expected_receive_period_in_days: 2,
+            rules: [
+              {
+                'type' => 'field<value',
+                'value' => '200.0',
+                'path' => 'price',
+              }
+            ],
+            keep_event: 'true'
+          },
+          user: users(:bob)
+        )
+      }
+
+      let(:valid_params) {
+        {
+          name: 'Example',
+          schedule: 'never',
+          options: {
+            'action' => '{% if target.id == agent_id %}configure{% endif %}',
+            'configure_options' => {
+              'rules' => [
+                {
+                  'type' => 'field<value',
+                  'value' => "{{price}}",
+                  'path' => 'price',
+                }
+              ]
+            }
+          },
+          user: users(:bob),
+          control_targets: [target, real_target]
+        }
+      }
+
+      it "should conditionally configure targets interpolating agent attributes" do
+        expect {
+          event = Event.new
+          event.agent = agents(:bob_website_agent)
+          event.payload = {
+            'price' => '198.0',
+            'agent_id' => real_target.id
+          }
+          agent.receive([event])
+        }.to change {
+          real_target.options['rules'][0]['value']
+        }.from('200.0').to('198.0') & not_change {
+          target.options
+        }
+      end
     end
   end
 end
