83c3962b04ab0be6e11ee9eb0cdc7f7080a22890
douniwan5788 <douniwan5788@gmail.com>
2018-04-26 13:02:46 +0800

    add ssl support for mail config

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -4,6 +4,7 @@ ActionMailer::Base.smtp_settings = {
   domain: ENV['SMTP_DOMAIN'],
   authentication: ENV['SMTP_AUTHENTICATION'] == 'none' ? nil : ENV['SMTP_AUTHENTICATION'] || "plain",
   enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO'] == 'true',
+  ssl: ENV['SMTP_SSL'] == 'true',
   user_name: ENV['SMTP_USER_NAME'] == 'none' ? nil : ENV['SMTP_USER_NAME'].presence,
   password: ENV['SMTP_USER_NAME'] == 'none' ? nil : ENV['SMTP_PASSWORD'].presence,
   openssl_verify_mode: ENV['SMTP_OPENSSL_VERIFY_MODE'].presence,
