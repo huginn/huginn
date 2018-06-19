9ef1df36a46c4affbffa1795ca8b7763672a3ea4
Dominik Sander <git@dsander.de>
2018-06-19 17:47:36 +0200

    Not not initialize bootsnap when running as root
    Fixes the bootsnap cache not being readable and writeable by the huginn
    user afterwards.
    
    In situations where during the manual installation upgrade process `sudo
    bundle exec rake:production:*` was run before running a rake task as the
    huginn user the bootsnap cache directory was propagated by the root user
    which then causes exceptions when running any command as the huginn
    user.
    
     #2315

--- config/boot.rb
+++ config/boot.rb
@@ -4,4 +4,4 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require 'bootsnap/setup' if Process.euid != 0 # Speed up boot time by caching expensive operations.
