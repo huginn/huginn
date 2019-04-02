3bd9c15d18606e803222c0ddbd2eade1b8c7783e
Brian Petro <brian@plexm.com>
2019-04-02 16:52:17 -0400

    Add category_filter to data output
    Add the `category_filter` URL query parameter to enable filtering XML results by category.

--- app/models/agents/data_output_agent.rb
+++ app/models/agents/data_output_agent.rb
@@ -27,9 +27,12 @@ module Agents
           * `ttl` - A value for the \\<ttl\\> element in RSS output. (default: `60`)
           * `ns_media` - Add [yahoo media namespace](https://en.wikipedia.org/wiki/Media_RSS) in output xml
           * `ns_itunes` - Add [itunes compatible namespace](http://lists.apple.com/archives/syndication-dev/2005/Nov/msg00002.html) in output xml
-          * `rss_content_type` - Content-Type for RSS output (default: `application/rss+xml`)
+          * `rss_content_type` - Content-Type for RSS output (default: `application/rss+xml, application/rdf+xml;q=0.8, application/atom+xml;q=0.6, application/xml;q=0.4, text/xml;q=0.4`)
           * `response_headers` - An object with any custom response headers. (example: `{"Access-Control-Allow-Origin": "*"}`)
           * `push_hubs` - Set to a list of PubSubHubbub endpoints you want to publish an update to every time this agent receives an event. (default: none)  Popular hubs include [Superfeedr](https://pubsubhubbub.superfeedr.com/) and [Google](https://pubsubhubbub.appspot.com/).  Note that publishing updates will make your feed URL known to the public, so if you want to keep it secret, set up a reverse proxy to serve your feed via a safe URL and specify it in `template.self`.
+          * `template.item.category` (optional), use with `category_filter` URL parameter. When `category_filter` is set to "example", then only events where the interpolated value of category results in "example" will be included in the output.
+
+
 
         If you'd like to output RSS tags with attributes, such as `enclosure`, use something like the following in your `template`:
 
@@ -174,7 +177,7 @@ module Agents
     end
 
     def rss_content_type
-      interpolated['rss_content_type'].presence || 'application/rss+xml'
+      interpolated['rss_content_type'].presence || 'application/rss+xml, application/rdf+xml;q=0.8, application/atom+xml;q=0.6, application/xml;q=0.4, text/xml;q=0.4'
     end
 
     def xml_namespace
@@ -261,19 +264,22 @@ module Agents
       interpolate_with('events' => source_events) do
         items = source_events.map do |event|
           interpolated = interpolate_options(options['template']['item'], event)
-          interpolated['guid'] = {'_attributes' => {'isPermaLink' => 'false'},
-                                  '_contents' => interpolated['guid'].presence || event.id}
-          date_string = interpolated['pubDate'].to_s
-          date =
-            begin
-              Time.zone.parse(date_string)  # may return nil
-            rescue => e
-              error "Error parsing a \"pubDate\" value \"#{date_string}\": #{e.message}"
-              nil
-            end || event.created_at
-          interpolated['pubDate'] = date.rfc2822.to_s
-          interpolated
+          if !params['category_filter'].present? || (params['category_filter'] == interpolated['category']) 
+            interpolated['guid'] = {'_attributes' => {'isPermaLink' => 'false'},
+                                    '_contents' => interpolated['guid'].presence || event.id}
+            date_string = interpolated['pubDate'].to_s
+            date =
+              begin
+                Time.zone.parse(date_string)  # may return nil
+              rescue => e
+                error "Error parsing a \"pubDate\" value \"#{date_string}\": #{e.message}"
+                nil
+              end || event.created_at
+            interpolated['pubDate'] = date.rfc2822.to_s
+            interpolated
+          end
         end
+        items.compact! # compact to deal with nil values
 
         now = Time.now
 
