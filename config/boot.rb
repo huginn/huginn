2efdf4cdcb0ecfbfba3da5c258ef6ffd1bcf4758
Dominik Sander <git@dsander.de>
2019-10-01 20:43:53 +0200

    Upgrade to Rails 6

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,3 @@
-require 'rubygems'
-
-# Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
