5cd1fe098ecffca1af468c9f973ecc78b9190059
Dominik Sander <git@dsander.de>
2019-10-03 12:46:30 +0200

    Fix deprecation warnings from our code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+require 'utils'
+
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
