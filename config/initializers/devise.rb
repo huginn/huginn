5cbcb7e4c4c145a27b7a6e4161190a0793af4f59
Akinori MUSHA <knu@idaemons.org>
2020-03-16 03:04:23 +0900

    Enable the OmniAuth logger

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,6 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth_path_prefix = "/auth"
+
+  OmniAuth.config.logger = Rails.logger
 end
