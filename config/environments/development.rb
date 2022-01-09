88d4c8cbf33b83d720c185314fb02139cd5bd54c
roylez <me@roylez.info>
2022-01-09 13:56:41 +1000

    turn off consider_all_request_local again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
