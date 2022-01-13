5bb1e689fc31ecbfd95cfa72c9f1632d5f1ed476
Akinori MUSHA <knu@idaemons.org>
2022-01-14 00:59:24 +0900

    Give DOCKER_IMAGE and DOCKERFILE default values

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-set -ev
+set -e
 
+: ${DOCKER_IMAGE:=huginn/huginn}
 : ${DOCKER_IMAGE_TAG:=${GITHUB_SHA:-$(git rev-parse HEAD)}}
+: ${DOCKERFILE:=docker/multi-process/Dockerfile}
 
-bin/docker_wrapper build -t $DOCKER_IMAGE -f $DOCKERFILE .
+bin/docker_wrapper build -t "$DOCKER_IMAGE" -f "$DOCKERFILE" .
 
 if [[ "$1" == --push ]]; then
   [[ -n "$DOCKER_USER" && -n "$DOCKER_IMAGE_TAG" ]]
   docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
-  docker tag $DOCKER_IMAGE $DOCKER_IMAGE:$DOCKER_IMAGE_TAG
-  docker push $DOCKER_IMAGE
-  docker push $DOCKER_IMAGE:$DOCKER_IMAGE_TAG
+  docker tag "$DOCKER_IMAGE" "$DOCKER_IMAGE:$DOCKER_IMAGE_TAG"
+  docker push "$DOCKER_IMAGE"
+  docker push "$DOCKER_IMAGE:$DOCKER_IMAGE_TAG"
 fi
 
-if [[ $DOCKER_IMAGE == "huginn/huginn-single-process" ]]; then
+if [[ "$DOCKER_IMAGE" == "huginn/huginn-single-process" ]]; then
   DOCKER_IMAGE=huginn/huginn-test DOCKERFILE=docker/test/Dockerfile ./build_docker_image.sh
 fi
