39d9f6e9cb68cae6ba83efe899b42a92f0c64f69
Akinori MUSHA <knu@idaemons.org>
2023-02-19 18:22:27 +0900

    Enable the 6.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Huginn
     Dotenv.overload File.expand_path('../../spec/env.test', __FILE__) if Rails.env.test?
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
