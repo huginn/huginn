52cee481014c0f4c51ecd756a9b2e5281d9e257b
Akinori MUSHA <knu@idaemons.org>
2023-03-11 23:01:54 +0900

    Improve Twitter agents
    - TwitterFavorites, TwitterSearchAgent, and TwitterUserAgent can now dry run.
    - Correct event descriptions; the key should be `full_text` instead of `text` except for TwitterStreamAgent.
    - Events now include `expanded_text` in which shortened t.co URLs are expanded to their original URLs.
    - Refactor and reformat code.

--- app/concerns/twitter_concern.rb
+++ app/concerns/twitter_concern.rb
@@ -7,15 +7,23 @@ module TwitterConcern
     validate :validate_twitter_options
     valid_oauth_providers :twitter
 
-    gem_dependency_check { defined?(Twitter) && Devise.omniauth_providers.include?(:twitter) && ENV['TWITTER_OAUTH_KEY'].present? && ENV['TWITTER_OAUTH_SECRET'].present? }
+    gem_dependency_check {
+      defined?(Twitter) &&
+        Devise.omniauth_providers.include?(:twitter) &&
+        ENV['TWITTER_OAUTH_KEY'].present? &&
+        ENV['TWITTER_OAUTH_SECRET'].present?
+    }
   end
 
   def validate_twitter_options
     unless twitter_consumer_key.present? &&
-      twitter_consumer_secret.present? &&
-      twitter_oauth_token.present? &&
-      twitter_oauth_token_secret.present?
-      errors.add(:base, "Twitter consumer_key, consumer_secret, oauth_token, and oauth_token_secret are required to authenticate with the Twitter API.  You can provide these as options to this Agent, or as Credentials with the same names, but starting with 'twitter_'.")
+        twitter_consumer_secret.present? &&
+        twitter_oauth_token.present? &&
+        twitter_oauth_token_secret.present?
+      errors.add(
+        :base,
+        "Twitter consumer_key, consumer_secret, oauth_token, and oauth_token_secret are required to authenticate with the Twitter API.  You can provide these as options to this Agent, or as Credentials with the same names, but starting with 'twitter_'."
+      )
     end
   end
 
@@ -44,6 +52,56 @@ module TwitterConcern
     end
   end
 
+  HTML_ENTITIES = {
+    '&amp;' => '&',
+    '&lt;' => '<',
+    '&gt;' => '>',
+  }
+  RE_HTML_ENTITIES = Regexp.union(HTML_ENTITIES.keys)
+
+  def format_tweet(tweet)
+    attrs =
+      case tweet
+      when Twitter::Tweet
+        tweet.attrs
+      when Hash
+        if tweet.key?(:id)
+          tweet
+        else
+          tweet.deep_symbolize_keys
+        end
+      else
+        raise TypeError, "Unexpected tweet type: #{tweet.class}"
+      end
+
+    text = (attrs[:full_text] || attrs[:text])&.dup or return attrs
+
+    expanded_text = text.dup.tap { |text|
+      attrs.dig(:entities, :urls)&.reverse_each do |entity|
+        from, to = entity[:indices]
+        text[from...to] = entity[:expanded_url]
+      end
+    }
+    text.gsub!(RE_HTML_ENTITIES, HTML_ENTITIES)
+    expanded_text.gsub!(RE_HTML_ENTITIES, HTML_ENTITIES)
+
+    if attrs[:text]
+      {
+        **attrs,
+        text: text,
+        expanded_text: expanded_text,
+      }
+    else
+      {
+        **attrs,
+        full_text: text,
+        expanded_text: expanded_text,
+      }
+    end
+  end
+
+  module_function :format_tweet
+
   module ClassMethods
     def twitter_dependencies_missing
       if ENV['TWITTER_OAUTH_KEY'].blank? || ENV['TWITTER_OAUTH_SECRET'].blank?
@@ -51,6 +109,33 @@ module TwitterConcern
       elsif !defined?(Twitter) || !Devise.omniauth_providers.include?(:twitter)
         "## Include the `twitter`, `omniauth-twitter`, and `cantino-twitter-stream` gems in your Gemfile to use Twitter Agents."
       end
+    end
+
+    def tweet_event_description(text_key, extra_fields = nil)
+      <<~MD.indent(4)
+        {
+          #{extra_fields&.indent(2)}// ... every Tweet field, including ...
+          // Huginn automatically decodes "&lt;", "&gt;", and "&amp;" to "<", ">", and "&".
+          "#{text_key}": "something https://t.co/XXXX",
+          "user": {
+            "name": "Mr. Someone",
+            "screen_name": "Someone",
+            "location": "Vancouver BC Canada",
+            "description":  "...",
+            "followers_count": 486,
+            "friends_count": 1983,
+            "created_at": "Mon Aug 29 23:38:14 +0000 2011",
+            "time_zone": "Pacific Time (US & Canada)",
+            "statuses_count": 3807,
+            "lang": "en"
+          },
+          "retweet_count": 0,
+          "entities": ...
+          "lang": "en",
+          // Huginn adds this field, expanding all shortened t.co URLs in "#{text_key}".
+          "expanded_text": "something https://example.org/foo/bar"
+        }
+      MD
     end
   end
 end
--- app/models/agents/twitter_user_agent.rb
+++ app/models/agents/twitter_user_agent.rb
@@ -2,6 +2,7 @@ module Agents
   class TwitterUserAgent < Agent
     include TwitterConcern
 
+    can_dry_run!
     cannot_receive_events!
 
     description <<-MD
@@ -16,7 +17,7 @@ module Agents
       To follow your own home timeline set `choose_home_time_line` to `true`.
 
       Set `include_retweets` to `false` to not include retweets (default: `true`)
-      
+
       Set `exclude_replies` to `true` to exclude replies (default: `false`)
 
       Set `expected_update_period_in_days` to the maximum amount of time that you'd expect to pass between Events being created by this Agent.
@@ -24,27 +25,10 @@ module Agents
       Set `starting_at` to the date/time (eg. `Mon Jun 02 00:38:12 +0000 2014`) you want to start receiving tweets from (default: agent's `created_at`)
     MD
 
-    event_description <<-MD
-      Events are the raw JSON provided by the [Twitter API](https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline). Should look something like:
-          {
-             ... every Tweet field, including ...
-            "full_text": "something",
-            "user": {
-              "name": "Mr. Someone",
-              "screen_name": "Someone",
-              "location": "Vancouver BC Canada",
-              "description":  "...",
-              "followers_count": 486,
-              "friends_count": 1983,
-              "created_at": "Mon Aug 29 23:38:14 +0000 2011",
-              "time_zone": "Pacific Time (US & Canada)",
-              "statuses_count": 3807,
-              "lang": "en"
-            },
-            "retweet_count": 0,
-            "entities": ...
-            "lang": "en"
-          }
+    event_description <<~MD
+      Events are the raw JSON provided by the [Twitter API v1.1](https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline) with slight modifications.  They should look something like this:
+
+      #{tweet_event_description('full_text')}
     MD
 
     default_schedule "every_1h"
@@ -64,56 +48,77 @@ module Agents
     end
 
     def validate_options
-      errors.add(:base, "expected_update_period_in_days is required") unless options['expected_update_period_in_days'].present?
-      errors.add(:base, "username is required") if options['username'].blank? && !boolify(options['choose_home_time_line'])
+      if options[:expected_update_period_in_days].blank?
+        errors.add(:base, "expected_update_period_in_days is required")
+      end
 
-      if options[:include_retweets].present? && !%w[true false].include?(options[:include_retweets])
-        errors.add(:base, "include_retweets must be a boolean value string (true/false)")
+      if !boolify(options[:choose_home_time_line]) && options[:username].blank?
+        errors.add(:base, "username is required")
+      end
+
+      if options[:include_retweets].present? && !%w[true false].include?(options[:include_retweets].to_s)
+        errors.add(:base, "include_retweets must be a boolean value (true/false)")
       end
 
       if options[:starting_at].present?
-        Time.parse(options[:starting_at]) rescue errors.add(:base, "Error parsing starting_at")
+        begin
+          Time.parse(options[:starting_at])
+        rescue StandardError
+          errors.add(:base, "Error parsing starting_at")
+        end
       end
-    end
-
-    def starting_at
-      if interpolated[:starting_at].present?
-        Time.parse(interpolated[:starting_at]) rescue created_at
-      else
-        created_at
-      end
-    end
-
-    def choose_home_time_line?
-      boolify(interpolated['choose_home_time_line'])
-    end
-
-    def include_retweets?
-      interpolated[:include_retweets] != "false"
-    end
-    
-    def exclude_replies?
-      boolify(interpolated[:exclude_replies]) || false
     end
 
     def check
-      since_id = memory['since_id'] || nil
-      opts = {:count => 200, :include_rts => include_retweets?, :exclude_replies => exclude_replies?, :include_entities => true, :contributor_details => true, tweet_mode: 'extended'}
-      opts.merge! :since_id => since_id unless since_id.nil?
+      opts = {
+        count: 200,
+        include_rts: include_retweets?,
+        exclude_replies: exclude_replies?,
+        include_entities: true,
+        contributor_details: true,
+        tweet_mode: 'extended',
+        since_id: memory[:since_id].presence,
+      }.compact
 
-      if choose_home_time_line?
-        tweets = twitter.home_timeline(opts)
-      else
-        tweets = twitter.user_timeline(interpolated['username'], opts)
-      end
+      tweets =
+        if choose_home_time_line?
+          twitter.home_timeline(opts)
+        else
+          twitter.user_timeline(interpolated[:username], opts)
+        end
 
       tweets.each do |tweet|
-        if tweet.created_at >= starting_at
-          memory['since_id'] = tweet.id if !memory['since_id'] || (tweet.id > memory['since_id'])
+        next unless tweet.created_at >= starting_at
 
-          create_event :payload => tweet.attrs
-        end
+        memory[:since_id] = [tweet.id, *memory[:since_id]].max
+
+        create_event(payload: format_tweet(tweet))
       end
+    end
+
+    private
+
+    def starting_at
+      if interpolated[:starting_at].present?
+        begin
+          Time.parse(interpolated[:starting_at])
+        rescue StandardError
+        end
+      end || created_at || Time.now # for dry-running
+    end
+
+    def choose_home_time_line?
+      boolify(interpolated[:choose_home_time_line])
+    end
+
+    def include_retweets?
+      # default to true
+      boolify(interpolated[:include_retweets]) != false
+    end
+
+    def exclude_replies?
+      # default to false
+      boolify(interpolated[:exclude_replies]) || false
     end
   end
 end
