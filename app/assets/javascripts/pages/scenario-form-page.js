2806941ffeb73f1a32c913d51de337071d344ece
Akinori MUSHA <knu@idaemons.org>
2024-11-04 01:30:30 +0900

    Upgrade font-awesome from 4 to 6

--- app/assets/javascripts/pages/scenario-form-page.js
+++ app/assets/javascripts/pages/scenario-form-page.js
@@ -6,12 +6,12 @@ this.ScenarioFormPage = class ScenarioFormPage {
   format(icon) {
     const originalOption = icon.element;
     return (
-      '<i class="fa ' + $(originalOption).data("icon") + '"></i> ' + icon.text
+      '<i class="fa-solid ' + $(originalOption).data("icon") + '"></i> ' + icon.text
     );
   }
 
   enabledSelect2() {
-    return $(".select2-fountawesome-icon").select2({
+    return $(".select2-fontawesome-icon").select2({
       width: "100%",
       formatResult: this.format,
     });
