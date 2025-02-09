7668359370d0a0fae681d1885b6ad5c9db0fb888
James Jeffers <jdjeffers@protonmail.com>
2025-02-09 10:18:01 -0500

    Updates serialize options for latest ActiveRecord::Base#serialize, bumps Puma to get around https://github.com/puma/puma/issues/3531.

--- app/concerns/json_serialized_field.rb
+++ app/concerns/json_serialized_field.rb
@@ -6,35 +6,35 @@ module JsonSerializedField
   module ClassMethods
     def json_serialize(*fields)
       fields.each do |field|
-        class_eval <<-CODE
-          serialize :#{field}, JsonWithIndifferentAccess
+        class_eval <<~CODE, __FILE__, __LINE__ + 1
+                    serialize :#{field}, coder: JsonWithIndifferentAccess
 
-          validate :#{field}_has_no_errors
+                    validate :#{field}_has_no_errors
 
-          def #{field}=(input)
-            @#{field}_assignment_error = false
-            case input
-              when String
-                if input.strip.length == 0
-                  self[:#{field}] = ActiveSupport::HashWithIndifferentAccess.new
-                else
-                  json = JSON.parse(input) rescue nil
-                  if json
-                    self[:#{field}] = ActiveSupport::HashWithIndifferentAccess.new(json)
-                  else
-                    @#{field}_assignment_error = "was assigned invalid JSON"
-                  end
-                end
-              when Hash
-                self[:#{field}] = ActiveSupport::HashWithIndifferentAccess.new(input)
-              else
-                @#{field}_assignment_error = "cannot be set to an instance of \#{input.class}"
-            end
-          end
+                    def #{field}=(input)
+                      @#{field}_assignment_error = false
+          case input
+                        when String
+                          if input.strip.length == 0
+                            self[:#{field}] = ActiveSupport::HashWithIndifferentAccess.new
+                          else
+                            json = JSON.parse(input) rescue nil
+                            if json
+                              self[:#{field}] = ActiveSupport::HashWithIndifferentAccess.new(json)
+                            else
+                              @#{field}_assignment_error = "was assigned invalid JSON"
+                            end
+                          end
+                        when Hash
+                          self[:#{field}] = ActiveSupport::HashWithIndifferentAccess.new(input)
+                        else
+                          @#{field}_assignment_error = "cannot be set to an instance of \#{input.class}"
+                      end
+                    end
 
-          def #{field}_has_no_errors
-            errors.add(:#{field}, @#{field}_assignment_error) if @#{field}_assignment_error
-          end
+                    def #{field}_has_no_errors
+                      errors.add(:#{field}, @#{field}_assignment_error) if @#{field}_assignment_error
+                    end
         CODE
       end
     end
