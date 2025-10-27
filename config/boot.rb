488026bfe08ea8d78cc8e251bc7c2740a4900b33
Akinori Musha <knu@idaemons.org>
2025-10-27 17:36:39 +0900

    Explicitly require the logger gem and set autoload
    ActuveSupport 7.0 seems to have a bug in its initialization process.
    
    ```
    NameError: uninitialized constant ActiveSupport::LoggerThreadSafeLevel::Logger (NameError)
    ```

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,5 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+autoload :Logger, "logger"
