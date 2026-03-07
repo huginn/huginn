a8a83faf9ce0d257aaf54a7f09fcc2010e611e6b
Akinori Musha <knu@idaemons.org>
2026-03-08 04:57:42 +0900

    Stabilize agent creation feature spec

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -3,6 +3,8 @@ module FeatureHelpers
     agent_name = search[/\A.*?Agent\b/] || search
     select2(agent_name, search:, from: "Type")
 
+    expect(page).to have_no_css("#agent-spinner", visible: true)
+
     # Wait for all parts of the Agent form to load:
     expect(page).to have_css("div.function_buttons") # Options editor
     expect(page).to have_css(".well.description > p") # Markdown description
