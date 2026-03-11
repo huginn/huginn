51783e4fc153a97b5d231c83d023144a7968a404
Akinori Musha <knu@idaemons.org>
2026-03-12 01:34:32 +0900

    Replace Unicorn with single-threaded Puma
    - Switch runtime and deployment entrypoints from Unicorn to single-threaded Puma
    - Keep clustered Puma available via WEB_CONCURRENCY while defaulting threads to 1
    - Update Docker, Heroku, OpenShift, nginx, Capistrano, and manual deployment assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :deploy_to, '/home/huginn'
 set :log_level, :info
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('.env', 'Procfile', 'config/unicorn.rb')
+set :linked_files, fetch(:linked_files, []).push('.env', 'Procfile')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
