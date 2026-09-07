ee18f299b722aba6e8c08299099006e97c6bbcd9
Akinori Musha <knu@idaemons.org>
2026-09-07 18:46:53 +0900

    Enable Devise paranoid mode
    Respond to password reset, unlock and confirmation requests the same
    way whether or not the given login exists, so that these forms cannot
    be used to enumerate accounts.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -73,7 +73,7 @@ Devise.setup do |config|
   # It will change confirmation, password recovery and other workflows
   # to behave the same regardless if the e-mail provided was right or wrong.
   # Does not affect registerable.
-  # config.paranoid = true
+  config.paranoid = true
 
   # By default Devise will store the user in session. You can skip storage for
   # particular strategies by setting this option.
